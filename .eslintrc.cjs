f3488aa611caecc45bcf26a3f0703fb2c19f5596
Josh Goldberg <git@joshuakgoldberg.com>
2022-10-02 00:32:46 -0400

    hotfix: move .prettierrc to the right folder, whoops

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,29 +1,29 @@
 module.exports = {
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    "plugin:@typescript-eslint/strict",
-    "prettier",
-  ],
-  overrides: [
-    {
-      files: "**/*.test.ts",
-      rules: {
-        "@typescript-eslint/no-unsafe-assignment": "off",
-        "@typescript-eslint/no-unsafe-call": "off",
-      },
-    },
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    tsconfigRootDir: __dirname,
-    project: ["./tsconfig.json"],
-  },
-  plugins: ["@typescript-eslint", "simple-import-sort"],
-  root: true,
-  rules: {
-    "simple-import-sort/exports": "error",
-    "simple-import-sort/imports": "error",
-  },
+	extends: [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:@typescript-eslint/recommended-requiring-type-checking",
+		"plugin:@typescript-eslint/strict",
+		"prettier",
+	],
+	overrides: [
+		{
+			files: "**/*.test.ts",
+			rules: {
+				"@typescript-eslint/no-unsafe-assignment": "off",
+				"@typescript-eslint/no-unsafe-call": "off",
+			},
+		},
+	],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		tsconfigRootDir: __dirname,
+		project: ["./tsconfig.json"],
+	},
+	plugins: ["@typescript-eslint", "simple-import-sort"],
+	root: true,
+	rules: {
+		"simple-import-sort/exports": "error",
+		"simple-import-sort/imports": "error",
+	},
 };

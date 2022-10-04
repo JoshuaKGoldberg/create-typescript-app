bf523f294abb396eb0fc3b59269eaee58e6ea73f
Josh Goldberg <git@joshuakgoldberg.com>
2022-10-04 15:45:40 -0400

    More to uncomment lol

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -37,7 +37,7 @@ module.exports = {
 	plugins: ["@typescript-eslint", "simple-import-sort", "typescript-sort-keys"],
 	root: true,
 	rules: {
-		// "simple-import-sort/exports": "error",
-		// "simple-import-sort/imports": "error",
+		"simple-import-sort/exports": "error",
+		"simple-import-sort/imports": "error",
 	},
 };

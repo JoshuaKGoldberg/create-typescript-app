b0625d051e2eec584ecfd4d335db925785dcf7a4
Josh Goldberg <git@joshuakgoldberg.com>
2022-10-04 15:38:58 -0400

    chore: add eslint-plugin-jsonc with sort-keys rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,31 @@
 module.exports = {
 	extends: [
-		"eslint:recommended",
-		"plugin:@typescript-eslint/recommended",
-		"plugin:@typescript-eslint/recommended-requiring-type-checking",
-		"plugin:@typescript-eslint/strict",
-		"plugin:typescript-sort-keys/recommended",
-		"prettier",
+		// "eslint:recommended",
+		// "plugin:typescript-sort-keys/recommended",
+		// "prettier",
 	],
 	overrides: [
 		{
+			extends: [
+				// "plugin:@typescript-eslint/recommended",
+				// "plugin:@typescript-eslint/recommended-requiring-type-checking",
+				// "plugin:@typescript-eslint/strict",
+			],
+			files: ["**/*.ts", "**/*.tsx"],
+		},
+		{
+			files: "*.json",
+			parser: "jsonc-eslint-parser",
+			rules: {
+				"jsonc/sort-keys": "error",
+			},
+			extends: ["plugin:jsonc/recommended-with-json"],
+		},
+		{
 			files: "**/*.test.ts",
 			rules: {
-				"@typescript-eslint/no-unsafe-assignment": "off",
-				"@typescript-eslint/no-unsafe-call": "off",
+				// "@typescript-eslint/no-unsafe-assignment": "off",
+				// "@typescript-eslint/no-unsafe-call": "off",
 			},
 		},
 	],
@@ -24,7 +37,7 @@ module.exports = {
 	plugins: ["@typescript-eslint", "simple-import-sort", "typescript-sort-keys"],
 	root: true,
 	rules: {
-		"simple-import-sort/exports": "error",
-		"simple-import-sort/imports": "error",
+		// "simple-import-sort/exports": "error",
+		// "simple-import-sort/imports": "error",
 	},
 };

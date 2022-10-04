13a5f2e45ee665d95b29a1add56198a7c65972ad
Josh Goldberg <git@joshuakgoldberg.com>
2022-10-04 15:44:02 -0400

    Put back ESLint config, and do build failure

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,17 @@
 module.exports = {
 	extends: [
-		// "eslint:recommended",
-		// "plugin:typescript-sort-keys/recommended",
-		// "prettier",
+		"eslint:recommended",
+		"plugin:typescript-sort-keys/recommended",
+		"prettier",
 	],
 	overrides: [
 		{
 			extends: [
-				// "plugin:@typescript-eslint/recommended",
-				// "plugin:@typescript-eslint/recommended-requiring-type-checking",
-				// "plugin:@typescript-eslint/strict",
+				"plugin:@typescript-eslint/recommended",
+				"plugin:@typescript-eslint/recommended-requiring-type-checking",
+				"plugin:@typescript-eslint/strict",
 			],
-			files: ["**/*.ts", "**/*.tsx"],
+			files: ["**/*.{ts,tsx}"],
 		},
 		{
 			files: "*.json",
@@ -24,8 +24,8 @@ module.exports = {
 		{
 			files: "**/*.test.ts",
 			rules: {
-				// "@typescript-eslint/no-unsafe-assignment": "off",
-				// "@typescript-eslint/no-unsafe-call": "off",
+				"@typescript-eslint/no-unsafe-assignment": "off",
+				"@typescript-eslint/no-unsafe-call": "off",
 			},
 		},
 	],

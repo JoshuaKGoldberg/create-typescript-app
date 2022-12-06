4b2729ee8368cd492f76d16c95cd08c23a80019c
Josh Goldberg <git@joshuakgoldberg.com>
2022-12-05 22:16:14 -0500

    Remove unnecessary files

--- script/clover2lcov.js
+++
@@ -1,7 +0,0 @@
-import { toLcov } from "clover2lcov";
-import { promises as fs } from "fs";
-
-const original = await fs.readFile("./coverage/clover.xml");
-const converted = await toLcov(original);
-
-await fs.writeFile("./coverage/lcov.info", converted);

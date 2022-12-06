e411b35c0eb7f3b41706817569cb081c199c5fb2
Josh Goldberg <git@joshuakgoldberg.com>
2022-12-05 22:13:25 -0500

    Remove dist/

--- dist/greet.js
+++
@@ -1,7 +0,0 @@
-export function greet(options) {
-    const { logger = console.log.bind(console), message, times = 1, } = typeof options === "string" ? { message: options } : options;
-    for (let i = 0; i < times; i += 1) {
-        logger(message);
-    }
-}
-//# sourceMappingURL=greet.js.map
--- dist/types.d.ts
+++
@@ -1,6 +0,0 @@
-export interface GreetOptions {
-    logger?: (message: string) => void;
-    message: string;
-    times?: number;
-}
-//# sourceMappingURL=types.d.ts.map

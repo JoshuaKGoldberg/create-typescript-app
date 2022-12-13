408dc52d2aedca16d4c057291eb4ae19b2a8f88c
Josh Goldberg <git@joshuakgoldberg.com>
2022-12-13 14:58:37 -0500

    chore: fix coverage computation to ignore types (#101)

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,10 @@ export default defineConfig({
 			all: true,
 			include: ["src"],
 			reporter: ["html", "lcov"],
+
+			// c8 reports types-only lines as uncovered
+			// https://github.com/JoshuaKGoldberg/template-typescript-node-package/issues/100
+			provider: "istanbul",
 		},
 	},
 });

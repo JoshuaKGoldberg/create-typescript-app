ba6aff5360e38dfa28f3bcef74650576f0bbc412
Josh Goldberg <git@joshuakgoldberg.com>
2022-12-20 15:54:44 -0500

    feat: use Octokit to populate more settings

--- script/setup.js
+++ script/setup.js
@@ -1,7 +1,26 @@
 /* global $ */
+import { parseArgs } from "node:util";
+
 import chalk from "chalk";
 import { promises as fs } from "fs";
+import { Octokit } from "octokit";
 import prettier from "prettier";
+import replace from "replace-in-file";
+
+console.log("wat");
+
+let prompt;
+
+try {
+	console.log("a");
+	prompt = require("enquirer").prompt;
+	console.log("b");
+} catch (error) {
+	console.error({ error });
+	process.exitCode = 1;
+}
+
+console.log("Successfully imported prompt", { prompt });
 
 let caughtError;
 
@@ -17,14 +36,13 @@ try {
 	);
 	console.log();
 
-	console.log(chalk.gray`Setting up temporary devDependency packages...`);
-	await $`pnpm add enquirer replace-in-file yargs -D`;
-	console.log(chalk.gray`✔️ Done.`);
-	console.log();
-
 	console.log(chalk.gray`Checking gh auth status...`);
+	let auth;
 	try {
 		await $`gh auth status`;
+
+		// Todo (Josh, off stream): make sure this is working 🙃
+		auth = await $`gh auth token`;
 	} catch (error) {
 		throw new Error(error.stderr);
 	}
@@ -32,14 +50,11 @@ try {
 	console.log(chalk.gray`✔️ Done.`);
 	console.log();
 
-	const { parseArgs } = require("node:util");
-	const { prompt } = require("enquirer");
-
 	const { values } = parseArgs({
 		args: process.argv.slice(2),
 		options: {
 			description: { type: "string" },
-			organization: { type: "string" },
+			owner: { type: "string" },
 			repository: { type: "string" },
 			title: { type: "string" },
 		},
@@ -70,9 +85,9 @@ try {
 		"What will the Sentence Case title of the repository be?"
 	);
 
-	const organization = await getPrefillOrPromptedValue(
-		"organization",
-		"What organization or user will the repository be under?"
+	const owner = await getPrefillOrPromptedValue(
+		"owner",
+		"What owner or user will the repository be under?"
 	);
 
 	const description = await getPrefillOrPromptedValue(
@@ -112,12 +127,10 @@ try {
 		)
 	);
 
-	const replace = require("replace-in-file");
-
 	for (const [from, to, files = ["./.github/**/*", "./*.*"]] of [
 		[existingPackage.description, description],
 		["Template TypeScript Node Package", title],
-		["JoshuaKGoldberg", organization],
+		["JoshuaKGoldberg", owner],
 		["template-typescript-node-package", repository],
 		[/"setup": ".*",/, ``, "./package.json"],
 		[
@@ -177,9 +190,59 @@ try {
 	console.log(chalk.gray`✔️ Done.`);
 
 	console.log();
-	console.log(chalk.gray`Hydrating repository settings...`);
+	console.log(chalk.gray`Hydrating initial repository settings...`);
 
-	await $`gh repo edit --delete-branch-on-merge --description ${description} --enable-auto-merge --enable-rebase-merge=false --enable-squash-merge`;
+	console.log(chalk.gray`✔️ Done.`);
+
+	const octokit = new Octokit({ auth });
+
+	octokit.rest.repos.update({
+		allow_auto_merge: true,
+		allow_rebase_merge: false,
+		allow_squash_merge: true,
+		default_branch: "main",
+		delete_branch_on_merge: true,
+		description,
+		has_wiki: false,
+		owner,
+		repo: repository,
+	});
+
+	console.log();
+	console.log(chalk.gray`Hydrating branch protection settings...`);
+
+	await octokit.request(
+		`PUT /repos/${owner}/${repository}/branches/main/protection`,
+		{
+			allow_deletions: false,
+			allow_force_pushes: true,
+			allow_fork_syncing: true,
+			block_creations: false,
+			branch: "main",
+			enforce_admins: false,
+			lock_branch: true,
+			owner,
+			repo: repository,
+			required_conversation_resolution: true,
+			required_linear_history: false,
+			required_status_checks: {
+				checks: [
+					{ context: "build" },
+					{ context: "compliance" },
+					{ context: "lint" },
+					{ context: "markdown" },
+					{ context: "package" },
+					{ context: "packages" },
+					{ context: "prettier" },
+					{ context: "prune" },
+					{ context: "release" },
+					{ context: "spelling" },
+					{ context: "test" },
+				],
+				strict: false,
+			},
+		}
+	);
 
 	console.log(chalk.gray`✔️ Done.`);
 
@@ -194,8 +257,10 @@ try {
 	caughtError = error;
 } finally {
 	console.log();
-	console.log(chalk.gray`Cleaning up temporary devDependency packages...`);
-	await $`pnpm remove enquirer replace-in-file yargs -D`;
+	console.log(
+		chalk.gray`Removing devDependency packages only used for setup...`
+	);
+	await $`pnpm remove enquirer octokit replace-in-file -D`;
 	console.log(chalk.gray`✔️ Done.`);
 }
 

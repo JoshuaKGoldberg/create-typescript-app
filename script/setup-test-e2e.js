dda6f2b57c6f9c52d2c39cd3ae5fc413db5e96d4
Josh Goldberg <git@joshuakgoldberg.com>
2022-12-23 15:38:00 -0500

    A couple e2e fixups

--- script/setup-test-e2e.js
+++ script/setup-test-e2e.js
@@ -9,7 +9,9 @@ const owner = "NewOwnerTest";
 const title = "New Title Test";
 const repository = "new-repository-test";
 
-await $`pnpm run setup --description ${description} --owner ${owner} --title ${title} --repository ${repository} --skip-api`;
+const result =
+	await $`pnpm run setup --description ${description} --owner ${owner} --title ${title} --repository ${repository} --skip-api`;
+console.log({ result });
 
 const newPackageJson = JSON.parse(
 	(await fs.readFile("./package.json")).toString()

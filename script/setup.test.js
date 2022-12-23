647bbb5864a53ea8ca37315f2a160336fc0d28a5
Josh Goldberg <git@joshuakgoldberg.com>
2022-12-23 15:32:07 -0500

    chore: end-to-end test setup script

---
+++ script/setup.test.js
@@ -0,0 +1,19 @@
+/* global $ */
+
+import { strict as assert } from "node:assert";
+
+import { promises as fs } from "fs";
+
+const description = "New Description Test";
+const owner = "NewOwnerTest";
+const title = "New Title Test";
+const repository = "new-repository-test";
+
+await $`pnpm run setup --description ${description} --owner ${owner} --title ${title} --repository ${repository} --skip-api`;
+
+const newPackageJson = JSON.parse(
+	(await fs.readFile("./package.json")).toString()
+);
+
+assert.equal(newPackageJson.description, description);
+assert.equal(newPackageJson.name, repository);

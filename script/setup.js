c87f390df86e079bfa15ba69a0f1e681f3df9fe1
Ron Braha <ron.braha@icloud.com>
2023-05-09 13:31:50 -0400

    fix: adjustments

--- script/setup.js
+++ script/setup.js
@@ -15,6 +15,15 @@ let exitCode = 0;
 let skipRestore = true;
 const s = prompts.spinner();
 
+function handlePromptCancel(value) {
+	if (prompts.isCancel(value)) {
+		prompts.cancel(
+			"Operation cancelled. Exiting setup - maybe another time? 👋"
+		);
+		process.exit(0);
+	}
+}
+
 try {
 	console.clear();
 	console.log(
@@ -32,14 +41,14 @@ try {
 			repository: { type: "string" },
 			title: { type: "string" },
 			"skip-api": { type: "boolean" },
-			"skip-uninstalls": { type: "boolean" },
 			"skip-restore": { type: "boolean" },
+			"skip-uninstalls": { type: "boolean" },
 		},
 		tokens: true,
 		strict: false,
 	});
 
-	skipRestore = Boolean(values["skip-restore"]);
+	skipRestore = values["skip-restore"];
 
 	async function getDefaultSettings() {
 		let gitRemoteFetch;
@@ -94,12 +103,7 @@ try {
 			},
 		});
 
-		if (prompts.isCancel(value)) {
-			prompts.cancel(
-				"Operation cancelled. Exiting setup - maybe another time? 👋"
-			);
-			process.exit(0);
-		}
+		handlePromptCancel(value);
 
 		return value;
 	}
@@ -578,11 +582,26 @@ try {
 
 	if (skipRestore) {
 		console.log();
+		console.log(chalk.gray`Skipping restoring local repository, as requested.`);
+		console.log();
 	} else {
 		const shouldRestore = await prompts.confirm({
-			message: "Do you want to restore the changes?",
+			message:
+				"Do you want to restore the repository to how it was before running setup?",
 		});
-		if (shouldRestore) await $`git restore .`;
+
+		handlePromptCancel(shouldRestore);
+
+		if (shouldRestore) {
+			console.log();
+			console.log(
+				chalk.gray`Resetting repository using`,
+				chalk.reset`git restore .`
+			);
+			await $`git restore .`;
+			console.log("Repository is reset. Ready to try again?");
+			console.log();
+		}
 	}
 
 	exitCode = 1;

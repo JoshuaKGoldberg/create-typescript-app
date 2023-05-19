8a7106138d9b6fc62a377b2835466826ec6677ed
Josh Goldberg <git@joshuakgoldberg.com>
2023-05-19 17:08:10 -0400

    Fleshed out e2e script

--- script/hydrate-test-e2e.js
+++ script/hydrate-test-e2e.js
@@ -1,45 +1,50 @@
-import { strict as assert } from "node:assert";
-
+import chalk from "chalk";
 import { $ } from "execa";
-import { promises as fs } from "fs";
-import { globby } from "globby";
 
-const description = "New Description Test";
-const repository = "new-repository-test";
+const { stdout: gitStatus } = await $`git status`;
+console.log(`Stdout from running \`git status\`:\n${gitStatus}`);
 
-const result = await $`node ./lib/hydrate/index.js`;
-console.log("Result from node ./lib/hydrate/index.js:", result);
+const indexOfUnstagedFilesMessage = gitStatus.indexOf(
+	"Changes not staged for commit:"
+);
+if (indexOfUnstagedFilesMessage === -1) {
+	throw new Error(
+		`Looks like hydrate didn't cause any file changes? That's ...probably incorrect? 😬`
+	);
+}
 
-const gitStatus = await $`git status`;
-console.log("Git status:", gitStatus);
+const filesExpectedToBeChanged = new Set([
+	".all-contributorsrc",
+	"README.md",
+	"knip.jsonc",
+	"package.json",
+	"pnpm-lock.yaml",
+]);
 
-// const newPackageJson = JSON.parse(
-// 	(await fs.readFile("./package.json")).toString()
-// );
-// console.log("New package JSON:", newPackageJson);
+const unstagedModifiedFiles = gitStatus
+	.slice(indexOfUnstagedFilesMessage)
+	.match(/modified: {3}(\S+)\n/g)
+	.map((match) => match.split(/\s+/g)[1])
+	.filter((filePath) => !filesExpectedToBeChanged.has(filePath));
 
-// assert.equal(newPackageJson.description, description);
-// assert.equal(newPackageJson.name, repository);
-
-// const files = await globby(["*.*", "**/*.*"], {
-// 	gitignore: true,
-// 	ignoreFiles: ["script/setup.js", "script/setup-test-e2e.js"],
-// });
-
-// for (const search of [
-// 	`/JoshuaKGoldberg/`,
-// 	"template-typescript-node-package",
-// ]) {
-// 	const { stdout } = await $`grep -i ${search} ${files}`;
-// 	assert.equal(
-// 		stdout,
-// 		`README.md:> 💙 This package is based on [@JoshuaKGoldberg](https://github.com/JoshuaKGoldberg)'s [template-typescript-node-package](https://github.com/JoshuaKGoldberg/template-typescript-node-package).`
-// 	);
-// }
-
-// try {
-// 	await $`pnpm run lint:knip`;
-// } catch (error) {
-// 	console.error("Error running lint:knip:", error);
-// 	process.exitCode = 1;
-// }
+if (unstagedModifiedFiles.length) {
+	console.log(
+		`Stdout from running \`git diff\`:${(await $`git diff HEAD`).stdout}`
+	);
+	console.error(
+		chalk.red(
+			[
+				"",
+				"Oh no! Running the hydrate script modified some files:",
+				...unstagedModifiedFiles.map((filePath) => ` - ${filePath}`),
+				"",
+				"That likely indicates changes made to the repository without",
+				"corresponding updates to templates in src/hydrate/creation.",
+				"",
+				"Please search for those file(s)' name(s) under src/hydrate for",
+				"the corresponding template and update those as well.",
+			].join("\n")
+		)
+	);
+	process.exitCode = 1;
+}

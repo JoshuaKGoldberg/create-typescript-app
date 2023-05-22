1f866951c86621b41a5035e667f7a669c159efd5
Josh Goldberg <git@joshuakgoldberg.com>
2023-05-22 17:54:43 +0300

    A more tolerant command

--- script/hydrate-test-e2e.js
+++ script/hydrate-test-e2e.js
@@ -23,6 +23,15 @@ const filesExpectedToBeChanged = new Set([
 	"knip.jsonc",
 	"package.json",
 	"pnpm-lock.yaml",
+	".eslintignore",
+	".eslintrc.cjs",
+	".github/DEVELOPMENT.md",
+	".github/workflows/release.yml",
+	".github/workflows/test.yml",
+	".gitignore",
+	".prettierignore",
+	".release-it.json",
+	"cspell.json",
 ]);
 
 const unstagedModifiedFiles = gitStatus
@@ -31,26 +40,29 @@ const unstagedModifiedFiles = gitStatus
 	.map((match) => match.split(/\s+/g)[1])
 	.filter((filePath) => !filesExpectedToBeChanged.has(filePath));
 
-console.log("Unstaged modified files are:", unstagedModifiedFiles);
+console.log("Unexpected modified files are:", unstagedModifiedFiles);
 
 if (unstagedModifiedFiles.length) {
+	const gitDiffCommand = `git diff HEAD -- ${unstagedModifiedFiles.join(" ")}`;
 	console.log(
-		`Stdout from running \`git diff\`:\n${(await $`git diff HEAD`).stdout}`
+		`Stdout from running \`${gitDiffCommand}\`:\n${
+			(await $(gitDiffCommand)).stdout
+		}`
 	);
 	console.error(
-		chalk.red(
-			[
-				"",
-				"Oh no! Running the hydrate script modified some files:",
-				...unstagedModifiedFiles.map((filePath) => ` - ${filePath}`),
-				"",
-				"That likely indicates changes made to the repository without",
-				"corresponding updates to templates in src/hydrate/creation.",
-				"",
-				"Please search for those file(s)' name(s) under src/hydrate for",
-				"the corresponding template and update those as well.",
-			].join("\n")
-		)
+		[
+			"",
+			"Oh no! Running the hydrate script modified some files:",
+			...unstagedModifiedFiles.map((filePath) => ` - ${filePath}`),
+			"",
+			"That likely indicates changes made to the repository without",
+			"corresponding updates to templates in src/hydrate/creation.",
+			"",
+			"Please search for those file(s)' name(s) under src/hydrate for",
+			"the corresponding template and update those as well.",
+		]
+			.map((line) => chalk.red(line))
+			.join("\n")
 	);
 	process.exitCode = 1;
 }

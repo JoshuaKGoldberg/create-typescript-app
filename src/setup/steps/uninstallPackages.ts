08fdbd03aa9fed3f21fde30b86c6701d56330695
Josh Goldberg <git@joshuakgoldberg.com>
2023-05-22 17:55:36 +0300

    Uninstall c8 too

--- src/setup/steps/uninstallPackages.ts
+++ src/setup/steps/uninstallPackages.ts
@@ -2,5 +2,5 @@ import { $ } from "execa";
 
 export async function uninstallPackages() {
 	await $`pnpm remove chalk execa js-yaml`;
-	await $`pnpm remove @clack/prompts @octokit/request-error @types/js-yaml @types/prettier all-contributors-cli globby octokit npm-user replace-in-file title-case tsx -D`;
+	await $`pnpm remove @clack/prompts @octokit/request-error @types/js-yaml @types/prettier all-contributors-cli c8 globby octokit npm-user replace-in-file title-case tsx -D`;
 }

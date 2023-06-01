19631d9bbcb5bb55ab619c36589dbcde43c2afac
Josh Goldberg <git@joshuakgoldberg.com>
2023-06-01 15:52:48 +0300

    How about git add -A

--- script/hydrate-test-e2e.js
+++ script/hydrate-test-e2e.js
@@ -10,6 +10,8 @@ await $({
 	stdio: "inherit",
 })`c8 -o ./coverage-hydrate -r html -r lcov node ./lib/hydrate/index.js  --description ${description} --owner ${owner} --title ${title} --repository ${repository} --skip-api --skip-install --skip-setup`;
 
+await $`git add -A`;
+
 const { stdout: gitStatus } = await $`git status`;
 console.log(`Stdout from running \`git status\`:\n${gitStatus}`);
 

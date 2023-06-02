186c3f82a2ab8992e062fbf17e431399da1453d9
Josh Goldberg ✨ <git@joshuakgoldberg.com>
2023-06-02 14:01:28 +0300

    feat: rename existing aliased labels (#497)
    ## PR Checklist
    
    - [x] Addresses an existing open issue: fixes #455
    - [x] That issue was marked as [`status: accepting
    prs`](https://github.com/JoshuaKGoldberg/template-typescript-node-package/issues?q=is%3Aopen+is%3Aissue+label%3A%22status%3A+accepting+prs%22)
    - [x] Steps in
    [CONTRIBUTING.md](https://github.com/JoshuaKGoldberg/template-typescript-node-package/blob/main/.github/CONTRIBUTING.md)
    were taken
    
    ## Overview
    
    Adds an `getExistingEquivalentLabel` function to check for common label
    aliases.
    
    See the default labels here:
    https://docs.github.com/en/issues/using-labels-and-milestones-to-track-work/managing-labels

---
+++ src/setup/steps/labels/getExistingEquivalentLabel.test.ts
@@ -0,0 +1,41 @@
+import { describe, expect, it } from "vitest";
+
+import { getExistingEquivalentLabel } from "./getExistingEquivalentLabel.js";
+
+describe("getExistingEquivalentLabel", () => {
+	it("returns undefined when there are no existing labels", () => {
+		const actual = getExistingEquivalentLabel([], "abc");
+
+		expect(actual).toBe(undefined);
+	});
+
+	it("returns undefined when no existing label matches", () => {
+		const actual = getExistingEquivalentLabel(["abc"], "def");
+
+		expect(actual).toBe(undefined);
+	});
+
+	it("returns the existing label when it matches by name", () => {
+		const actual = getExistingEquivalentLabel(["def", "abc", "ghi"], "abc");
+
+		expect(actual).toBe("abc");
+	});
+
+	it("returns the existing label when it matches excluding prefix", () => {
+		const actual = getExistingEquivalentLabel(
+			["abc: def", "abc", "ghi"],
+			"type: abc"
+		);
+
+		expect(actual).toBe("abc");
+	});
+
+	it("returns the existing label when it matches an alias", () => {
+		const actual = getExistingEquivalentLabel(
+			["abc: def", "enhancement", "ghi"],
+			"type: feature"
+		);
+
+		expect(actual).toBe("enhancement");
+	});
+});

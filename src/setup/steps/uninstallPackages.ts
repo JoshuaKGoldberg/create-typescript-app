fe6b2da42a90c408588bcd01d9a329ef1692951e
Navin Moorthy <navin007.a@gmail.com>
2023-06-13 13:12:51 +0530

    refactor(uninstallPackages): ♻ Rearrange order of uninstalled packages

--- src/setup/steps/uninstallPackages.ts
+++ src/setup/steps/uninstallPackages.ts
@@ -2,5 +2,5 @@ import { $ } from "execa";
 
 export async function uninstallPackages() {
 	await $`pnpm remove chalk execa js-yaml`;
-	await $`pnpm remove @clack/prompts @octokit/request-error @types/js-yaml @types/prettier all-contributors-cli c8 globby octokit npm-user replace-in-file title-case git-remote-origin-url git-url-parse @types/git-url-parse tsx -D`;
+	await $`pnpm remove @clack/prompts @octokit/request-error @types/git-url-parse @types/js-yaml @types/prettier all-contributors-cli c8 git-remote-origin-url git-url-parse globby npm-user octokit replace-in-file title-case tsx -D`;
 }

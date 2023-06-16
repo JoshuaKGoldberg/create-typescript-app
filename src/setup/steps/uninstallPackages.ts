7e0e77503bbc27aa01ae007e74e17976b473c4eb
Josh Goldberg <git@joshuakgoldberg.com>
2023-06-16 12:13:05 -0400

    fix: made @clack/prompts a full dependency

--- src/setup/steps/uninstallPackages.ts
+++ src/setup/steps/uninstallPackages.ts
@@ -1,6 +1,6 @@
 import { $ } from "execa";
 
 export async function uninstallPackages() {
-	await $`pnpm remove chalk execa js-yaml`;
-	await $`pnpm remove @clack/prompts @octokit/request-error @types/git-url-parse @types/js-yaml @types/prettier all-contributors-cli c8 git-remote-origin-url git-url-parse globby npm-user octokit replace-in-file title-case tsx -D`;
+	await $`pnpm remove @clack/prompts chalk execa js-yaml`;
+	await $`pnpm remove @octokit/request-error @types/git-url-parse @types/js-yaml @types/prettier all-contributors-cli c8 git-remote-origin-url git-url-parse globby npm-user octokit replace-in-file title-case tsx -D`;
 }

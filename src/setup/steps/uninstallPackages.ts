1b124cef35a37f58d9301f68f39ef50ccb2a8bff
Josh Goldberg <git@joshuakgoldberg.com>
2023-06-16 12:39:42 -0400

    fix: promote replace-in-file to full dependency

--- src/setup/steps/uninstallPackages.ts
+++ src/setup/steps/uninstallPackages.ts
@@ -1,7 +1,7 @@
 import { $ } from "execa";
 
 export async function uninstallPackages() {
-	await $`pnpm remove @clack/prompts chalk execa git-remote-origin-url git-url-parse js-yaml npm-user prettier title-case`;
-	await $`pnpm remove @octokit/request-error @types/git-url-parse @types/js-yaml @types/prettier all-contributors-cli c8 globby octokit replace-in-file tsx -D`;
+	await $`pnpm remove @clack/prompts chalk execa git-remote-origin-url git-url-parse js-yaml npm-user prettier replace-in-file title-case`;
+	await $`pnpm remove @octokit/request-error @types/git-url-parse @types/js-yaml @types/prettier all-contributors-cli c8 globby octokit tsx -D`;
 	await $`pnpm add prettier -D`;
 }

f1884616b4ce11d51fb138ca38ace7780c3db50b
Josh Goldberg <git@joshuakgoldberg.com>
2023-08-07 14:28:39 -0400

    feat: use tsup to build lib/

---
+++ tsup.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+	bundle: false,
+	clean: true,
+	dts: true,
+	entry: ["src/**/*.ts", "!src/**/*.test.*"],
+	format: "esm",
+	outDir: "lib",
+});

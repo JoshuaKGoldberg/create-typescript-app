8b60088854c53b8edc362436fb14f090cb22f424
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-08-14 13:16:36 -0400

    removed skip next line from prompts

--- src/shared/prompts.ts
+++ src/shared/prompts.ts
@@ -2,7 +2,6 @@ import * as prompts from "@clack/prompts";
 
 export function handleCancel() {
 	prompts.cancel("Operation cancelled. Exiting - maybe another time? 👋");
-	// eslint-disable-next-line n/no-process-exit
 	throw new Error("GitHub authentication failed.");
 }
 

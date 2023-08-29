00489b25d495ba2fd8bfc15b2b67e31b2d6bd73c
Josh Goldberg <git@joshuakgoldberg.com>
2023-08-29 14:43:50 -0400

    fix: chmod ug+x .husky/pre-commit after writing structure

--- src/steps/writing/writeStructure.ts
+++ src/steps/writing/writeStructure.ts
@@ -1,7 +1,12 @@
+import { $ } from "execa";
+
 import { Options } from "../../shared/types.js";
 import { createStructure } from "./creation/index.js";
 import { writeStructureWorker } from "./writeStructureWorker.js";
 
 export async function writeStructure(options: Options) {
 	await writeStructureWorker(await createStructure(options), ".");
+
+	// https://github.com/JoshuaKGoldberg/template-typescript-node-package/issues/718
+	await $`chmod ug+x .husky/pre-commit`;
 }

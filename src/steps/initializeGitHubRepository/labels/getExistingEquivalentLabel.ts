6966f8961f3aadc85fd464782a7b91e7c007a2a1
Josh Goldberg <git@joshuakgoldberg.com>
2023-08-31 11:52:54 -0400

    fix: allow existing labels (never gh label delete)

--- src/steps/initializeGitHubRepository/labels/getExistingEquivalentLabel.ts
+++ src/steps/initializeGitHubRepository/labels/getExistingEquivalentLabel.ts
@@ -3,18 +3,24 @@ const aliases = new Map([
 	["help wanted", "status: accepting prs"],
 ]);
 
+export interface GhLabelData {
+	color: string;
+	description: string;
+	name: string;
+}
+
 export function getExistingEquivalentLabel(
-	existingLabels: string[],
+	existingLabels: GhLabelData[],
 	outcomeLabel: string,
 ) {
 	const outcomeTrimmed = outcomeLabel.replace(/\w+: (\w+)/, "$1");
 
-	return existingLabels.find((existingLabel) => {
+	return existingLabels.find(({ name: existingName }) => {
 		return (
-			existingLabel === outcomeLabel ||
-			existingLabel === outcomeTrimmed ||
-			aliases.get(existingLabel) === outcomeLabel ||
-			existingLabel.replace(/\w+: (\w+)/, "$1") === outcomeLabel
+			existingName === outcomeLabel ||
+			existingName === outcomeTrimmed ||
+			aliases.get(existingName) === outcomeLabel ||
+			existingName.replace(/\w+: (\w+)/, "$1") === outcomeLabel
 		);
 	});
 }

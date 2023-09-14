6e9bdc70c801477c51fadab2ecf580fe142ffc86
nowyDEV <dominik.nowik@nordcloud.com>
2023-09-14 16:01:36 +0200

    add test case for successful run

--- src/shared/options/readOptions.test.ts
+++ src/shared/options/readOptions.test.ts
@@ -1,39 +1,79 @@
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, vi } from "vitest";
 
 import { readOptions } from "./readOptions.js";
 
+const emptyOptions = {
+	author: undefined,
+	base: undefined,
+	createRepository: undefined,
+	description: undefined,
+	email: undefined,
+	excludeCompliance: undefined,
+	excludeContributors: undefined,
+	excludeLintJson: undefined,
+	excludeLintKnip: undefined,
+	excludeLintMd: undefined,
+	excludeLintPackageJson: undefined,
+	excludeLintPackages: undefined,
+	excludeLintPerfectionist: undefined,
+	excludeLintSpelling: undefined,
+	excludeLintYml: undefined,
+	excludeReleases: undefined,
+	excludeRenovate: undefined,
+	excludeTests: undefined,
+	funding: undefined,
+	owner: undefined,
+	repository: undefined,
+	skipGitHubApi: false,
+	skipInstall: false,
+	skipRemoval: false,
+	skipRestore: undefined,
+	skipUninstall: false,
+	title: undefined,
+};
+
+const mockOptions = {
+	base: "prompt",
+	github: "mock.git",
+	repository: "mock.repository",
+};
+
+vi.mock("./getPrefillOrPromptedOption.js", () => ({
+	get getPrefillOrPromptedOption() {
+		return vi.fn(() => "mock");
+	},
+}));
+
+vi.mock("./ensureRepositoryExists.js", () => ({
+	get ensureRepositoryExists() {
+		return vi.fn(() => ({
+			github: mockOptions.github,
+			repository: mockOptions.repository,
+		}));
+	},
+}));
+
+vi.mock("./augmentOptionsWithExcludes.js", () => ({
+	get augmentOptionsWithExcludes() {
+		return vi.fn(() => ({ ...emptyOptions, ...mockOptions }));
+	},
+}));
+
 describe("readOptions", () => {
-	it("validates unsupported arguments and cancels the function", async () => {
+	it("validates unsupported argument and cancels the function", async () => {
 		expect(await readOptions(["--base", "true"])).toStrictEqual({
 			cancelled: true,
+			options: emptyOptions,
+		});
+	});
+
+	it("validates valid argument and continues function execution", async () => {
+		expect(await readOptions(["--base", mockOptions.base])).toStrictEqual({
+			cancelled: false,
+			github: mockOptions.github,
 			options: {
-				author: undefined,
-				base: undefined,
-				createRepository: undefined,
-				description: undefined,
-				email: undefined,
-				excludeCompliance: undefined,
-				excludeContributors: undefined,
-				excludeLintJson: undefined,
-				excludeLintKnip: undefined,
-				excludeLintMd: undefined,
-				excludeLintPackageJson: undefined,
-				excludeLintPackages: undefined,
-				excludeLintPerfectionist: undefined,
-				excludeLintSpelling: undefined,
-				excludeLintYml: undefined,
-				excludeReleases: undefined,
-				excludeRenovate: undefined,
-				excludeTests: undefined,
-				funding: undefined,
-				owner: undefined,
-				repository: undefined,
-				skipGitHubApi: false,
-				skipInstall: false,
-				skipRemoval: false,
-				skipRestore: undefined,
-				skipUninstall: false,
-				title: undefined,
+				...emptyOptions,
+				...mockOptions,
 			},
 		});
 	});

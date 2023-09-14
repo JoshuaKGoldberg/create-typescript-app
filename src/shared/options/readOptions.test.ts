f2f3a9b81be7a53491aa3bea6a75ed9b13ff8a9d
nowyDEV <dominik.nowik@nordcloud.com>
2023-09-14 14:22:20 +0200

    add test for zod validation

---
+++ src/shared/options/readOptions.test.ts
@@ -0,0 +1,40 @@
+import { describe, expect, it } from "vitest";
+
+import { readOptions } from "./readOptions.js";
+
+describe("readOptions", () => {
+	it("validates unusupported arguments and cancels the function", async () => {
+		expect(await readOptions(["--base", "true"])).toStrictEqual({
+			cancelled: true,
+			options: {
+				author: undefined,
+				base: undefined,
+				createRepository: undefined,
+				description: undefined,
+				email: undefined,
+				excludeCompliance: undefined,
+				excludeContributors: undefined,
+				excludeLintJson: undefined,
+				excludeLintKnip: undefined,
+				excludeLintMd: undefined,
+				excludeLintPackageJson: undefined,
+				excludeLintPackages: undefined,
+				excludeLintPerfectionist: undefined,
+				excludeLintSpelling: undefined,
+				excludeLintYml: undefined,
+				excludeReleases: undefined,
+				excludeRenovate: undefined,
+				excludeTests: undefined,
+				funding: undefined,
+				owner: undefined,
+				repository: undefined,
+				skipGitHubApi: false,
+				skipInstall: false,
+				skipRemoval: false,
+				skipRestore: undefined,
+				skipUninstall: false,
+				title: undefined,
+			},
+		});
+	});
+});

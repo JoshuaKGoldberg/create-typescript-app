a6909ef3a13ac9a4b6e9340f9533143dafbe2197
Josh Goldberg <git@joshuakgoldberg.com>
2023-09-20 17:54:21 -0400

    Corrected mock path

--- src/steps/writeReadme/index.test.ts
+++ src/steps/writeReadme/index.test.ts
@@ -17,7 +17,7 @@ vi.mock("node:fs/promises", () => ({
 
 const mockReadFileSafe = vi.fn();
 
-vi.mock("../shared/readFileSafe.js", () => ({
+vi.mock("../../shared/readFileSafe.js", () => ({
 	get readFileSafe() {
 		return mockReadFileSafe;
 	},
@@ -55,7 +55,7 @@ const options = {
 
 describe("writeReadme", () => {
 	it("writes a new file when the README.md does not yet exist", async () => {
-		mockReadFileSafe.mockResolvedValue(undefined);
+		mockReadFileSafe.mockResolvedValueOnce("");
 
 		await writeReadme(options);
 
@@ -115,7 +115,7 @@ describe("writeReadme", () => {
 	});
 
 	it("adds sections when the README.md already exists and is sparse", async () => {
-		mockReadFileSafe.mockResolvedValue(`# ${options.title}`);
+		mockReadFileSafe.mockResolvedValueOnce(`# ${options.title}`);
 
 		await writeReadme(options);
 
@@ -149,7 +149,70 @@ describe("writeReadme", () => {
 				</a>
 				<img alt=\\"Style: Prettier\\" src=\\"https://img.shields.io/badge/style-prettier-21bb42.svg\\" />
 				<img alt=\\"TypeScript: Strict\\" src=\\"https://img.shields.io/badge/typescript-strict-21bb42.svg\\" />
-			</p>
+			</p>e
+
+			## Contributors
+			<!-- spellchecker: disable -->
+			<!-- ALL-CONTRIBUTORS-LIST:START - Do not remove or modify this section -->
+			<!-- prettier-ignore-start -->
+			<!-- markdownlint-disable -->
+			<table>
+			<!-- (this will be filled in by all-contributors) -->
+			</table>
+			<!-- markdownlint-restore -->
+			<!-- prettier-ignore-end -->
+			<!-- ALL-CONTRIBUTORS-LIST:END -->
+			<!-- spellchecker: enable -->
+
+
+			<!-- You can remove this notice if you don't want it 🙂 no worries! -->
+
+			> 💙 This package is based on [@JoshuaKGoldberg](https://github.com/JoshuaKGoldberg)'s [create-typescript-app](https://github.com/JoshuaKGoldberg/create-typescript-app).
+			",
+			  ],
+			]
+		`);
+	});
+
+	it("adds all-contributors content when directed to and the indicator does not yet exist", async () => {
+		mockReadFileSafe.mockResolvedValueOnce(`# ${options.title}`);
+
+		await writeReadme({
+			...options,
+			excludeContributors: false,
+		});
+
+		expect(mockWriteFile.mock.calls).toMatchInlineSnapshot(`
+			[
+			  [
+			    "README.md",
+			    "<h1 align=\\"center\\">Test Title</h1>
+
+			<p align=\\"center\\">Test description.</p>
+
+			<p align=\\"center\\">
+				<a href=\\"#contributors\\" target=\\"_blank\\">
+			<!-- prettier-ignore-start -->
+			<!-- ALL-CONTRIBUTORS-BADGE:START - Do not remove or modify this section -->
+			<img alt=\\"All Contributors: 2\\" src=\\"https://img.shields.io/badge/all_contributors-17-21bb42.svg\\" />
+			<!-- ALL-CONTRIBUTORS-BADGE:END -->
+			<!-- prettier-ignore-end -->
+			</a>
+				<a href=\\"https://codecov.io/gh/TestOwner/test-repository\\" target=\\"_blank\\">
+					<img alt=\\"Codecov Test Coverage\\" src=\\"https://codecov.io/gh/TestOwner/test-repository/branch/main/graph/badge.svg\\"/>
+				</a>
+				<a href=\\"https://github.com/TestOwner/test-repository/blob/main/.github/CODE_OF_CONDUCT.md\\" target=\\"_blank\\">
+					<img alt=\\"Contributor Covenant\\" src=\\"https://img.shields.io/badge/code_of_conduct-enforced-21bb42\\" />
+				</a>
+				<a href=\\"https://github.com/TestOwner/test-repository/blob/main/LICENSE.md\\" target=\\"_blank\\">
+					<img alt=\\"License: MIT\\" src=\\"https://img.shields.io/github/license/TestOwner/test-repository?color=21bb42\\">
+				</a>
+				<a href=\\"https://github.com/sponsors/TestFunding\\" target=\\"_blank\\">
+					<img alt=\\"Sponsor: On GitHub\\" src=\\"https://img.shields.io/badge/sponsor-on_github-21bb42.svg\\" />
+				</a>
+				<img alt=\\"Style: Prettier\\" src=\\"https://img.shields.io/badge/style-prettier-21bb42.svg\\" />
+				<img alt=\\"TypeScript: Strict\\" src=\\"https://img.shields.io/badge/typescript-strict-21bb42.svg\\" />
+			</p>e
 
 			## Contributors
 			<!-- spellchecker: disable -->
@@ -175,7 +238,7 @@ describe("writeReadme", () => {
 	});
 
 	it("does not duplicate sections when the README.md already exists and has them", async () => {
-		mockReadFileSafe.mockResolvedValue(`<h1 align="center">Test Title</h1>
+		mockReadFileSafe.mockResolvedValueOnce(`<h1 align="center">Test Title</h1>
 
 <p align="center">Test description.</p>
 
@@ -255,9 +318,11 @@ describe("writeReadme", () => {
 				</a>
 				<img alt=\\"Style: Prettier\\" src=\\"https://img.shields.io/badge/style-prettier-21bb42.svg\\" />
 				<img alt=\\"TypeScript: Strict\\" src=\\"https://img.shields.io/badge/typescript-strict-21bb42.svg\\" />
+				<img alt=\\"Contributor Covenant\\" src=\\"https://img.shields.io/badge/code_of_conduct-enforced-21bb42\\" />
 			</p>
 
 			## Contributors
+
 			<!-- spellchecker: disable -->
 			<!-- ALL-CONTRIBUTORS-LIST:START - Do not remove or modify this section -->
 			<!-- prettier-ignore-start -->
@@ -265,8 +330,10 @@ describe("writeReadme", () => {
 			<table>
 			<!-- (this will be filled in by all-contributors) -->
 			</table>
+
 			<!-- markdownlint-restore -->
 			<!-- prettier-ignore-end -->
+
 			<!-- ALL-CONTRIBUTORS-LIST:END -->
 			<!-- spellchecker: enable -->
 

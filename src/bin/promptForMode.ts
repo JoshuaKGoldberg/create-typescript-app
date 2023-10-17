e61b4aec4d93b100c69a2d131f9fa98973e5be9f
Adarsh Jha <132337675+adarsh-jha-dev@users.noreply.github.com>
2023-10-17 08:03:32 +0530

    Update promptForMode.ts

--- src/bin/promptForMode.ts
+++ src/bin/promptForMode.ts
@@ -8,112 +8,115 @@ import { logLine } from "../shared/cli/lines.js";
 import { filterPromptCancel } from "../shared/prompts.js";
 import { Mode, PromptedOptions } from "../shared/types.js";
 
-const allowedModes = ["create", "initialize", "migrate"] satisfies Mode[];
+// Custom error class for invalid mode
+class InvalidModeError extends Error {
+    constructor(mode) {
+        super(`Unknown --mode: ${mode}. Allowed modes are: ${allowedModes.join(", ")}.`);
+        this.name = "InvalidModeError";
+    }
+}
 
+const allowedModes = ["create", "initialize", "migrate"] as Mode[];
+
+// Check if a value is a valid mode
 function isMode(input: boolean | string): input is Mode {
-	return allowedModes.includes(input as Mode);
+    return allowedModes.includes(input as Mode);
 }
 
 function label(base: string, text: string) {
-	return `${chalk.bold(base)} ${text}`;
+    return `${chalk.bold(base)} ${text}`;
 }
 
 export interface PromptedMode {
-	mode: Error | Mode | undefined;
-	options?: PromptedOptions;
+    mode: Mode | undefined;
+    options?: PromptedOptions;
 }
 
-export async function promptForMode(
-	input: boolean | string | undefined,
-): Promise<PromptedMode> {
-	if (input) {
-		if (!isMode(input)) {
-			return {
-				mode: new Error(
-					`Unknown --mode: ${input}. Allowed modes are: ${allowedModes.join(
-						", ",
-					)}.`,
-				),
-			};
-		}
+async function promptForMode(input: boolean | string | undefined): Promise<PromptedMode> {
+    // Validate input mode
+    if (input) {
+        if (!isMode(input)) {
+            throw new InvalidModeError(input);
+        }
+        return { mode: input };
+    }
 
-		return { mode: input };
-	}
+    const dir = await fs.readdir(".");
 
-	const dir = await fs.readdir(".");
+    if (dir.length === 0) {
+        const mode = filterPromptCancel(
+            (await prompts.select({
+                message: chalk.blue("How would you like to use the template?"),
+                options: [
+                    {
+                        label: label(
+                            "create",
+                            "a new repository in the current empty directory",
+                        ),
+                        value: "create-current",
+                    },
+                    {
+                        label: label("create", "a new repository in a new child directory"),
+                        value: "create-child",
+                    },
+                ],
+            })) as string,
+        );
 
-	if (dir.length === 0) {
-		const mode = filterPromptCancel(
-			(await prompts.select({
-				message: chalk.blue("How would you like to use the template?"),
-				options: [
-					{
-						label: label(
-							"create",
-							"a new repository in the current empty directory",
-						),
-						value: "create-current",
-					},
-					{
-						label: label("create", "a new repository in a new child directory"),
-						value: "create-child",
-					},
-				],
-			})) as string,
-		);
+        const directory = path.basename(process.cwd());
 
-		const directory = path.basename(process.cwd());
+        return {
+            mode: "create",
+            ...(mode === "create-current" && {
+                options: {
+                    directory: ".",
+                    repository: directory,
+                },
+            }),
+        };
+    }
 
-		return {
-			mode: "create",
-			...(mode === "create-current" && {
-				options: {
-					directory: ".",
-					repository: directory,
-				},
-			}),
-		};
-	}
+    if (dir.includes(".git")) {
+        return {
+            mode: filterPromptCancel(
+                (await prompts.select({
+                    initialValue: "migrate" as Mode,
+                    message: chalk.blue("How would you like to use the template?"),
+                    options: [
+                        {
+                            label: label("create", "a new repository in a child directory"),
+                            value: "create",
+                        },
+                        {
+                            label: label(
+                                "initialize",
+                                "a freshly cloned repository in the current directory",
+                            ),
+                            value: "initialize",
+                        },
+                        {
+                            label: label(
+                                "migrate",
+                                "the existing repository in the current directory",
+                            ),
+                            value: "migrate",
+                        },
+                    ],
+                })) as Mode,
+            ),
+        };
+    }
 
-	if (dir.includes(".git")) {
-		return {
-			mode: filterPromptCancel(
-				(await prompts.select({
-					initialValue: "migrate" as Mode,
-					message: chalk.blue("How would you like to use the template?"),
-					options: [
-						{
-							label: label("create", "a new repository in a child directory"),
-							value: "create",
-						},
-						{
-							label: label(
-								"initialize",
-								"a freshly cloned repository in the current directory",
-							),
-							value: "initialize",
-						},
-						{
-							label: label(
-								"migrate",
-								"the existing repository in the current directory",
-							),
-							value: "migrate",
-						},
-					],
-				})) as Mode | symbol,
-			),
-		};
-	}
+    logLine();
+    logLine(
+        chalk.gray(
+            "Defaulting to --mode create because the directory contains children and isn't a Git repository.",
+        ),
+    );
 
-	logLine();
-	logLine(
-		chalk.gray(
-			"Defaulting to --mode create because the directory contains children and isn't a Git repository.",
-		),
-	);
-
-	return {
-		mode: "create",
-	};
+    return {
+        mode: "create",
+    };
 }
+
+export { promptForMode };

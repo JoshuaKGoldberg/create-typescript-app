1b8e7149d50a02d7d2e5924fe2b1eeba2b96e304
Dominic Duffin <dominicduffin@outlook.com>
2023-10-21 17:56:52 +0100

    Attempt to mock an invalid package json file for testing

--- src/bin/packageJson.test.ts
+++ src/bin/packageJson.test.ts
@@ -1,12 +1,27 @@
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, vi } from "vitest";
 
 import packageData from "../../package.json" assert { type: "json" };
 import { getVersionFromPackageJson } from "./packageJson.js";
 
 describe("getVersionFromPackageJson", () => {
-	it("returns the current version number when given a valid package.json URL", async () => {
+	it("returns the current version number from the package.json", async () => {
 		const version = await getVersionFromPackageJson();
 
 		expect(version).toBe(packageData.version);
+	});
+
+	it("throws an error when there is no version number", async () => {
+		vi.mock("../shared/readFileSafeAsJson", async (importOriginal) => {
+			await importOriginal();
+			return {
+				readFileSafeAsJson: vi.fn(() => {
+					return {};
+				}),
+			};
+		});
+
+		await expect(() => getVersionFromPackageJson()).rejects.toEqual(
+			new Error("Cannot find version number"),
+		);
 	});
 });

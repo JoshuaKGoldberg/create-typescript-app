c68a4a6cd7aafa4cb65c7637c8c33533825b63c7
Graham Vasquez <graham@gvasquez.dev>
2023-10-30 14:35:33 -0400

    add additional test for `readOptions` function

--- src/shared/options/readOptions.test.ts
+++ src/shared/options/readOptions.test.ts
@@ -111,6 +111,11 @@ vi.mock("./createOptionDefaults/index.js", () => ({
 	},
 }));
 
+const readPackageData = vi.hoisted(() => vi.fn());
+vi.mock("../packages.js", () => ({
+	readPackageData,
+}));
+
 describe("readOptions", () => {
 	it("returns a cancellation when an arg is invalid", async () => {
 		const validationResult = z
@@ -516,6 +521,42 @@ describe("readOptions", () => {
 				guide: undefined,
 				logo: undefined,
 				mode: "create",
+				offline: true,
+				owner: "mock",
+				skipAllContributorsApi: true,
+				skipGitHubApi: true,
+				title: "mock",
+			},
+		});
+	});
+
+	it("infers base from package scripts during migration", async () => {
+		readPackageData.mockImplementationOnce(() =>
+			Promise.resolve({
+				scripts: {
+					build: "build",
+					lint: "lint",
+					test: "test",
+				},
+			}),
+		);
+		expect(await readOptions(["--offline"], "migrate")).toStrictEqual({
+			cancelled: false,
+			github: mockOptions.github,
+			options: {
+				...emptyOptions,
+				...mockOptions,
+				access: "public",
+				base: "minimum",
+				description: "mock",
+				directory: "mock",
+				email: {
+					github: "mock",
+					npm: "mock",
+				},
+				guide: undefined,
+				logo: undefined,
+				mode: "migrate",
 				offline: true,
 				owner: "mock",
 				skipAllContributorsApi: true,

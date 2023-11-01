5fe93c393435fa2eecdfc2f8dc67c74367cc71b4
Dominic Duffin <dominicduffin@outlook.com>
2023-11-01 22:11:49 +0000

    Revise package.json test

--- src/bin/packageJson.test.ts
+++ src/bin/packageJson.test.ts
@@ -3,6 +3,14 @@ import { describe, expect, it, vi } from "vitest";
 import packageData from "../../package.json" assert { type: "json" };
 import { getVersionFromPackageJson } from "./packageJson.js";
 
+const mockReadFileSafeAsJson = vi.fn();
+
+vi.mock("../shared/readFileSafeAsJson", () => ({
+	get readFileSafeAsJson() {
+		return mockReadFileSafeAsJson;
+	},
+}));
+
 describe("getVersionFromPackageJson", () => {
 	it("returns the current version number from the package.json", async () => {
 		const version = await getVersionFromPackageJson();
@@ -11,14 +19,7 @@ describe("getVersionFromPackageJson", () => {
 	});
 
 	it("throws an error when there is no version number", async () => {
-		vi.mock("../shared/readFileSafeAsJson", async (importOriginal) => {
-			await importOriginal();
-			return {
-				readFileSafeAsJson: vi.fn(() => {
-					return {};
-				}),
-			};
-		});
+		mockReadFileSafeAsJson.mockResolvedValue({});
 
 		await expect(() => getVersionFromPackageJson()).rejects.toEqual(
 			new Error("Cannot find version number"),

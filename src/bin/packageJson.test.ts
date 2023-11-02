68ea2df89a0135db513b6ce27f7ae60c149ad9ad
Dominic Duffin <dominicduffin@outlook.com>
2023-11-02 19:17:03 +0000

    Comment failing test

--- src/bin/packageJson.test.ts
+++ src/bin/packageJson.test.ts
@@ -5,6 +5,11 @@ import { getVersionFromPackageJson } from "./packageJson.js";
 
 const mockReadFileSafeAsJson = vi.fn();
 
+/* 
+Mock the readFileSafeAsJson function that is called in getVersionFromPackageJson().
+
+Modeled on readDefaultsFromDevelopment.test.ts line 7
+*/
 vi.mock("../shared/readFileSafeAsJson", () => ({
 	get readFileSafeAsJson() {
 		return mockReadFileSafeAsJson;
@@ -18,9 +23,15 @@ describe("getVersionFromPackageJson", () => {
 		expect(version).toBe(packageData.version);
 	});
 
+	// Failing test
 	it("throws an error when there is no version number", async () => {
+		/*
+			First calling the mocked function similar to line 16 of
+			readDefaultFromDevelopment.test.ts
+		*/
 		mockReadFileSafeAsJson.mockResolvedValue({});
 
+		// Test error case. This passes if I move line 15 of packageJson.ts to line 10.
 		await expect(() => getVersionFromPackageJson()).rejects.toEqual(
 			new Error("Cannot find version number"),
 		);

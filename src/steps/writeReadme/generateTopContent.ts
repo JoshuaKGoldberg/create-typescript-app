37313b5552cb9a8bb1729ca16b4fd0adf957b6cd
Shraddha <mailmeatshraddha@gmail.com>
2023-11-16 11:15:40 -0500

    fix: remove badge links underlines on hover

--- src/steps/writeReadme/generateTopContent.ts
+++ src/steps/writeReadme/generateTopContent.ts
@@ -24,39 +24,29 @@ export function generateTopContent(options: Options, existingBadges: string[]) {
 	for (const [badgeLine, existingMatcher] of [
 		[
 			!options.excludeAllContributors &&
-				`<a href="#contributors" target="_blank">
-<!-- prettier-ignore-start -->
-<!-- ALL-CONTRIBUTORS-BADGE:START - Do not remove or modify this section -->
-<img alt="All Contributors: 2" src="https://img.shields.io/badge/all_contributors-17-21bb42.svg" />
-<!-- ALL-CONTRIBUTORS-BADGE:END -->
-<!-- prettier-ignore-end -->
-</a>`,
+				`<!-- prettier-ignore-start -->
+				<!-- ALL-CONTRIBUTORS-BADGE:START - Do not remove or modify this section -->
+				<a href="#contributors" target="_blank"><img alt="All Contributors: 2" src="https://img.shields.io/badge/all_contributors-17-21bb42.svg" /></a>
+				<!-- ALL-CONTRIBUTORS-BADGE:END -->
+				<!-- prettier-ignore-end -->`,
 			/ALL-CONTRIBUTORS-BADGE:START/,
 		],
 		[
 			!options.excludeTests &&
-				`<a href="https://codecov.io/gh/${options.owner}/${options.repository}" target="_blank">
-		<img alt="Codecov Test Coverage" src="https://codecov.io/gh/${options.owner}/${options.repository}/branch/main/graph/badge.svg"/>
-	</a>`,
+				`<a href="https://codecov.io/gh/${options.owner}/${options.repository}" target="_blank"><img alt="Codecov Test Coverage" src="https://codecov.io/gh/${options.owner}/${options.repository}/branch/main/graph/badge.svg"/></a>`,
 			/https:\/\/codecov\.io\/gh/,
 		],
 		[
-			`<a href="https://github.com/${options.owner}/${options.repository}/blob/main/.github/CODE_OF_CONDUCT.md" target="_blank">
-		<img alt="Contributor Covenant" src="https://img.shields.io/badge/code_of_conduct-enforced-21bb42" />
-	</a>`,
+			`<a href="https://github.com/${options.owner}/${options.repository}/blob/main/.github/CODE_OF_CONDUCT.md" target="_blank"><img alt="Contributor Covenant" src="https://img.shields.io/badge/code_of_conduct-enforced-21bb42" /></a>`,
 			/CODE_OF_CONDUCT\.md/,
 		],
 		[
-			`<a href="https://github.com/${options.owner}/${options.repository}/blob/main/LICENSE.md" target="_blank">
-		<img alt="License: MIT" src="https://img.shields.io/github/license/${options.owner}/${options.repository}?color=21bb42">
-	</a>`,
+			`<a href="https://github.com/${options.owner}/${options.repository}/blob/main/LICENSE.md" target="_blank"><img alt="License: MIT" src="https://img.shields.io/github/license/${options.owner}/${options.repository}?color=21bb42"></a>`,
 			/LICENSE\.(md|txt)/,
 		],
 		[
 			options.funding &&
-				`<a href="https://github.com/sponsors/${options.funding}" target="_blank">
-		<img alt="Sponsor: On GitHub" src="https://img.shields.io/badge/sponsor-on_github-21bb42.svg" />
-	</a>`,
+				`<a href="https://github.com/sponsors/${options.funding}" target="_blank"><img alt="Sponsor: On GitHub" src="https://img.shields.io/badge/sponsor-on_github-21bb42.svg" /></a>`,
 			/github.+sponsors/,
 		],
 		[

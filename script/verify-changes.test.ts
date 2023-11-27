cc7d20fb78fc1af7251805e6eaf0d7052df60743
Mohammad Bagher Abiyat <zorofight94@gmail.com>
2023-11-27 10:00:37 +0330

    add vitest test file

--- script/verify-changes.test.ts
+++ script/verify-changes.test.ts
@@ -5,11 +5,12 @@ import { test } from "vitest";
 test("knip.jsonc", async () => {
 	const content = await fs.readFile("knip.jsonc", "utf-8");
 	const knipJSONC: Record<string, unknown> = JSON.parse(content);
-	console.log(content);
 
 	expect(Array.isArray(knipJSONC.project)).toBeTruthy();
 	expect(Array.isArray(knipJSONC.entry)).toBeTruthy();
 
-	expect(knipJSONC.$schema).toMatchInlineSnapshot();
+	expect(knipJSONC.$schema).toMatchInlineSnapshot(
+		'"https://unpkg.com/knip@latest/schema.json"',
+	);
 	expect(knipJSONC.ignoreExportsUsedInFile).toMatchInlineSnapshot();
 });

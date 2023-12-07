8884a4dde8709bb545248e2caf25fe2dcdf66c74
Josh Goldberg <git@joshuakgoldberg.com>
2023-12-07 13:42:32 -0500

    Fix for deprecations

--- src/bin/help.test.ts
+++ src/bin/help.test.ts
@@ -1,5 +1,5 @@
 import chalk from "chalk";
-import { SpyInstance, beforeEach, describe, expect, it, vi } from "vitest";
+import { MockInstance, beforeEach, describe, expect, it, vi } from "vitest";
 
 import { logHelpText } from "./help.js";
 
@@ -13,7 +13,7 @@ vi.mock("chalk", () => ({
 	default: makeProxy({}),
 }));
 
-let mockConsoleLog: SpyInstance;
+let mockConsoleLog: MockInstance;
 
 describe("logHelpText", () => {
 	beforeEach(() => {

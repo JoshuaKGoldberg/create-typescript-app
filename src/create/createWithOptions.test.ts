1def9c49074af44c2d973288b4c7bad00e79cdbd
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-12-27 23:27:09 -0500

    fixed test

--- src/create/createWithOptions.test.ts
+++ src/create/createWithOptions.test.ts
@@ -1,8 +1,13 @@
+import { $ } from "execa";
 import { Octokit } from "octokit";
 import { describe, expect, it, vi } from "vitest";
 
-import { withSpinner, withSpinners } from "../shared/cli/spinners.js";
+import { doesRepositoryExist } from "../shared/doesRepositoryExist.js";
 import { Options } from "../shared/types.js";
+import { addToolAllContributors } from "../steps/addToolAllContributors.js";
+import { finalizeDependencies } from "../steps/finalizeDependencies.js";
+import { initializeGitHubRepository } from "../steps/initializeGitHubRepository/index.js";
+import { runCommands } from "../steps/runCommands.js";
 import { writeReadme } from "../steps/writeReadme/index.js";
 import { writeStructure } from "../steps/writing/writeStructure.js";
 import { createWithOptions } from "./createWithOptions.js";
@@ -50,31 +55,113 @@ const options: Options = {
 	title: "Test Title",
 };
 
-const mockOctokit = new Octokit();
+vi.mock("execa", () => ({
+	$: vi.fn().mockResolvedValue({ stdout: "Mocked response" }),
+}));
 
+const mockOctokit = new Octokit();
 const github = {
 	auth: "auth-token",
 	octokit: mockOctokit,
 };
 
-vi.mock("../cli/spinners.ts", () => ({
-	withSpinner() {
-		return () => ({});
-	},
-}));
-
-vi.mock("../cli/spinners.ts", () => ({
-	withSpinners() {
-		return () => ({});
-	},
+vi.mock("../shared/cli/spinners.js", () => ({
+	withSpinner: vi.fn().mockImplementation(async (label: string, task) => {
+		return await task();
+	}),
+	withSpinners: vi.fn().mockImplementation(async (label: string, tasks) => {
+		for (const [_, task] of tasks) {
+			await task(); // Ensure each task is awaited
+		}
+	}),
 }));
 
 vi.mock("../steps/writing/writeStructure.js", () => ({
-	writeStructure: vi.fn().mockResolvedValue(undefined),
+	writeStructure: vi.fn().mockResolvedValue(null),
 }));
 
+vi.mock("../steps/writeReadme/index.js", () => ({
+	writeReadme: vi.fn().mockResolvedValue(null),
+}));
+
+vi.mock("../steps/finalizeDependencies.js", () => ({
+	finalizeDependencies: vi.fn().mockResolvedValue(null),
+}));
+
+vi.mock("../steps/runCommands.js", () => ({
+	runCommands: vi.fn().mockResolvedValue(null),
+}));
+
+vi.mock("../shared/doesRepositoryExist.js", () => ({
+	doesRepositoryExist: vi.fn().mockResolvedValue(true),
+}));
+
+vi.mock("../steps/initializeGitHubRepository/index.js", () => ({
+	initializeGitHubRepository: vi.fn().mockResolvedValue(true),
+}));
+
+vi.mock("../shared/getGitHubUserAsAllContributor.js", () => ({
+	getGitHubUserAsAllContributor: vi.fn().mockResolvedValue({
+		contributions: ["code", "doc"],
+		name: "Test User",
+	}),
+}));
+
+vi.mock("../steps/addToolAllContributors.js", () => ({
+	addToolAllContributors: vi.fn().mockResolvedValue(null),
+}));
+
+it("calls writeStructure with options", async () => {
+	await writeStructure(options);
+	expect(writeStructure).toHaveBeenCalledWith(options);
+});
+
+it("calls writeReadme with options", async () => {
+	await writeReadme(options);
+	expect(writeReadme).toHaveBeenCalledWith(options);
+});
+
+it("calls writeStructure and writeReadme with options", async () => {
+	await createWithOptions({ github, options });
+
+	expect(writeStructure).toHaveBeenCalledWith(options);
+	expect(writeReadme).toHaveBeenCalledWith(options);
+});
+
+it("directly calls addToolAllContributors with options", async () => {
+	await addToolAllContributors(options);
+	expect(addToolAllContributors).toHaveBeenCalledWith(options);
+});
+
+it("handles addToolAllContributors logic", async () => {
+	options.excludeAllContributors = false;
+	options.skipAllContributorsApi = false;
+
+	await createWithOptions({ github, options });
+	expect(addToolAllContributors).toHaveBeenCalledWith(options);
+});
+
+it("handles finalizeDependencies and runCommands logic", async () => {
+	options.skipInstall = false;
+
+	await createWithOptions({ github, options });
+
+	expect(finalizeDependencies).toHaveBeenCalledWith(options);
+	expect(runCommands).toHaveBeenCalled();
+});
+
+it("handles GitHub repository initialization", async () => {
+	await createWithOptions({ github, options });
+
+	expect(doesRepositoryExist).toHaveBeenCalledWith(github.octokit, options);
+	expect(initializeGitHubRepository).toHaveBeenCalledWith(
+		github.octokit,
+		options,
+	);
+});
+
 describe("createWithOptions", () => {
-	it("passes the options parameter to the writeStructure function", async () => {
+	it("calls writeStructure and writeReadme with options", async () => {
 		await createWithOptions({ github, options });
 
 		expect(writeStructure).toHaveBeenCalledWith(options);

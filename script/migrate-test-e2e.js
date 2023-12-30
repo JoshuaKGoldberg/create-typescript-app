ccdc584740d313f99bd828e4c86d1f6dcc91a279
Josh Goldberg <git@joshuakgoldberg.com>
2023-12-30 09:25:10 -0500

    chore: fill in newline for .github/DEVELOPMENT.md migration snapshot

--- script/migrate-test-e2e.js
+++ script/migrate-test-e2e.js
@@ -65,7 +65,8 @@ await fs.writeFile(
 // We manually add them back after hydration to clear them from Git diffs.
 await fs.appendFile(
 	".github/DEVELOPMENT.md",
-	originalDevelopment.slice(originalDevelopment.indexOf("## Setup Scripts")),
+	"\n" +
+		originalDevelopment.slice(originalDevelopment.indexOf("## Setup Scripts")),
 );
 
 // Ignore changes to the README.md all-contributor count and contributors table...

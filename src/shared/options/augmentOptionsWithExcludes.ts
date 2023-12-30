fa7e93e0368820e41ccca007dfb8e0c980fc2694
John Reilly <johnny_reilly@hotmail.com>
2023-12-30 10:02:26 +0000

    feat: tweak text
    Co-authored-by: Josh Goldberg ✨ <git@joshuakgoldberg.com>

--- src/shared/options/augmentOptionsWithExcludes.ts
+++ src/shared/options/augmentOptionsWithExcludes.ts
@@ -213,7 +213,7 @@ export async function augmentOptionsWithExcludes(
 					await prompts.multiselect({
 						initialValues: exclusionKeys,
 						message:
-							"Select the tooling portions you'd like to remove. All are enabled by default. You can press ↑ and ↓ on the keyboard to change the selected item, then space to select.",
+							"Select the tooling portions you'd like to remove. All are enabled by default. Press ↑ or ↓ to change the selected item, then space to select.",
 						options: Object.entries(exclusionDescriptions).map(
 							([value, { hint, label }]) => ({
 								hint,

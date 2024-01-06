4ecd099319ec1ebad81907076d8f49c5b3aa20ff
Josh Goldberg <git@joshuakgoldberg.com>
2024-01-06 09:26:54 -0500

    fix: allow chmod to fail in setup

--- src/steps/writing/writeStructure.ts
+++ src/steps/writing/writeStructure.ts
@@ -7,6 +7,10 @@ import { writeStructureWorker } from "./writeStructureWorker.js";
 export async function writeStructure(options: Options) {
 	await writeStructureWorker(await createStructure(options), ".");
 
-	// https://github.com/JoshuaKGoldberg/create-typescript-app/issues/718
-	await $`chmod ug+x .husky/pre-commit`;
+	try {
+		// https://github.com/JoshuaKGoldberg/create-typescript-app/issues/718
+		await $`chmod ug+x .husky/pre-commit`;
+	} catch {
+		// https://github.com/JoshuaKGoldberg/create-typescript-app/issues/1195
+	}
 }

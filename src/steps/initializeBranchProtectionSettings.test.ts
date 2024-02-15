3a8c7c03723eca5a50998270b75f15a7998ca69c
Josh Goldberg ✨ <git@joshuakgoldberg.com>
2024-02-15 11:29:18 -0500

    fix: gate branch protection settings on options exclusions (#1310)
    ## PR Checklist
    
    - [x] Addresses an existing open issue: fixes #1171
    - [x] That issue was marked as [`status: accepting
    prs`](https://github.com/JoshuaKGoldberg/create-typescript-app/issues?q=is%3Aopen+is%3Aissue+label%3A%22status%3A+accepting+prs%22)
    - [x] Steps in
    [CONTRIBUTING.md](https://github.com/JoshuaKGoldberg/create-typescript-app/blob/main/.github/CONTRIBUTING.md)
    were taken
    
    ## Overview
    
    This way, when creating/initializing/migrating a repository, branch
    protection settings won't be created for excluded options' CI flows.

--- src/steps/initializeBranchProtectionSettings.test.ts
+++ src/steps/initializeBranchProtectionSettings.test.ts
@@ -1,6 +1,7 @@
 import { Octokit } from "octokit";
 import { MockInstance, describe, expect, it, vi } from "vitest";
 
+import { Options } from "../shared/types.js";
 import { initializeBranchProtectionSettings } from "./initializeGitHubRepository/initializeBranchProtectionSettings.js";
 
 const createMockOctokit = (request: MockInstance) =>
@@ -8,7 +9,19 @@ const createMockOctokit = (request: MockInstance) =>
 		request,
 	}) as unknown as Octokit;
 
-const stubValues = { owner: "", repository: "" };
+const stubOptions = {
+	access: "public",
+	description: "",
+	directory: "",
+	email: {
+		github: "",
+		npm: "",
+	},
+	mode: "create",
+	owner: "",
+	repository: "",
+	title: "",
+} satisfies Options;
 
 describe("migrateBranchProtectionSettings", () => {
 	it("does not throw when the request receives a non-error response", async () => {
@@ -17,9 +30,64 @@ describe("migrateBranchProtectionSettings", () => {
 		await expect(
 			initializeBranchProtectionSettings(
 				createMockOctokit(mockRequest),
-				stubValues,
+				stubOptions,
 			),
 		).resolves.not.toThrow();
+
+		expect(mockRequest.mock.calls).toMatchInlineSnapshot(`
+			[
+			  [
+			    "PUT /repos///branches/main/protection",
+			    {
+			      "allow_deletions": false,
+			      "allow_force_pushes": true,
+			      "allow_fork_pushes": false,
+			      "allow_fork_syncing": true,
+			      "block_creations": false,
+			      "branch": "main",
+			      "enforce_admins": false,
+			      "owner": "",
+			      "repo": "",
+			      "required_conversation_resolution": true,
+			      "required_linear_history": false,
+			      "required_pull_request_reviews": null,
+			      "required_status_checks": {
+			        "checks": [
+			          {
+			            "context": "build",
+			          },
+			          {
+			            "context": "lint",
+			          },
+			          {
+			            "context": "prettier",
+			          },
+			          {
+			            "context": "compliance",
+			          },
+			          {
+			            "context": "lint_knip",
+			          },
+			          {
+			            "context": "lint_markdown",
+			          },
+			          {
+			            "context": "lint_packages",
+			          },
+			          {
+			            "context": "lint_spelling",
+			          },
+			          {
+			            "context": "test",
+			          },
+			        ],
+			        "strict": false,
+			      },
+			      "restrictions": null,
+			    },
+			  ],
+			]
+		`);
 	});
 
 	it("returns false when the request receives a 403 response", async () => {
@@ -27,7 +95,7 @@ describe("migrateBranchProtectionSettings", () => {
 
 		const actual = await initializeBranchProtectionSettings(
 			createMockOctokit(mockRequest),
-			stubValues,
+			stubOptions,
 		);
 
 		expect(actual).toBe(false);
@@ -40,8 +108,59 @@ describe("migrateBranchProtectionSettings", () => {
 		await expect(() =>
 			initializeBranchProtectionSettings(
 				createMockOctokit(mockRequest),
-				stubValues,
+				stubOptions,
 			),
 		).rejects.toBe(error);
+	});
+
+	it("doesn't create workflows for excluded options when specified", async () => {
+		const mockRequest = vi.fn().mockResolvedValue({ status: 200 });
+
+		await initializeBranchProtectionSettings(createMockOctokit(mockRequest), {
+			...stubOptions,
+			excludeCompliance: true,
+			excludeLintKnip: true,
+			excludeLintMd: true,
+			excludeLintPackages: true,
+			excludeLintSpelling: true,
+			excludeTests: true,
+		});
+
+		expect(mockRequest.mock.calls).toMatchInlineSnapshot(`
+			[
+			  [
+			    "PUT /repos///branches/main/protection",
+			    {
+			      "allow_deletions": false,
+			      "allow_force_pushes": true,
+			      "allow_fork_pushes": false,
+			      "allow_fork_syncing": true,
+			      "block_creations": false,
+			      "branch": "main",
+			      "enforce_admins": false,
+			      "owner": "",
+			      "repo": "",
+			      "required_conversation_resolution": true,
+			      "required_linear_history": false,
+			      "required_pull_request_reviews": null,
+			      "required_status_checks": {
+			        "checks": [
+			          {
+			            "context": "build",
+			          },
+			          {
+			            "context": "lint",
+			          },
+			          {
+			            "context": "prettier",
+			          },
+			        ],
+			        "strict": false,
+			      },
+			      "restrictions": null,
+			    },
+			  ],
+			]
+		`);
 	});
 });

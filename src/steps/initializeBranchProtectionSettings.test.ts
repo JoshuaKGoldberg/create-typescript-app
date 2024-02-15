ed3c19cfe07163da4367b6a6842ed6a3c2c17377
Josh Goldberg <git@joshuakgoldberg.com>
2024-02-15 11:25:42 -0500

    fix: gate branch protection settings on options exclusions

--- src/steps/initializeBranchProtectionSettings.test.ts
+++ src/steps/initializeBranchProtectionSettings.test.ts
@@ -1,6 +1,7 @@
 import { Octokit } from "octokit";
 import { MockInstance, describe, expect, it, vi } from "vitest";
 
+import { Options } from "../shared/types.js";
 import { initializeBranchProtectionSettings } from "./initializeGitHubRepository/initializeBranchProtectionSettings.js";
 
 const createMockOctokit = (request: MockInstance) =>
@@ -8,7 +9,19 @@ const createMockOctokit = (request: MockInstance) =>
 		request,
 	}) as unknown as Octokit;
 
-const stubValues = { owner: "", repository: "" };
+const stubValues = {
+	access: "public",
+	description: "",
+	directory: "",
+	email: {
+		github: "",
+		npm: "",
+	},
+	mode: "create",
+	owner: "",
+	repository: "",
+	title: "",
+} satisfies Options;
 
 describe("migrateBranchProtectionSettings", () => {
 	it("does not throw when the request receives a non-error response", async () => {
@@ -20,6 +33,61 @@ describe("migrateBranchProtectionSettings", () => {
 				stubValues,
 			),
 		).resolves.not.toThrow();
+
+		expect(mockRequest.mock.calls).toMatchInlineSnapshot(`
+			[
+			  [
+			    "PUT /repos///branches/main/protection",
+			    {
+			      "allow_deletions": false,
+			      "allow_force_pushes": true,
+			      "allow_fork_pushes": false,
+			      "allow_fork_syncing": true,
+			      "block_creations": false,
+			      "branch": "main",
+			      "enforce_admins": false,
+			      "owner": "",
+			      "repo": "",
+			      "required_conversation_resolution": true,
+			      "required_linear_history": false,
+			      "required_pull_request_reviews": null,
+			      "required_status_checks": {
+			        "checks": [
+			          {
+			            "context": "build",
+			          },
+			          {
+			            "context": "lint",
+			          },
+			          {
+			            "context": "prettier",
+			          },
+			          {
+			            "context": "compliance",
+			          },
+			          {
+			            "context": "lint_knip",
+			          },
+			          {
+			            "context": "lint_markdown",
+			          },
+			          {
+			            "context": "lint_packages",
+			          },
+			          {
+			            "context": "lint_spelling",
+			          },
+			          {
+			            "context": "test",
+			          },
+			        ],
+			        "strict": false,
+			      },
+			      "restrictions": null,
+			    },
+			  ],
+			]
+		`);
 	});
 
 	it("returns false when the request receives a 403 response", async () => {
@@ -43,5 +111,56 @@ describe("migrateBranchProtectionSettings", () => {
 				stubValues,
 			),
 		).rejects.toBe(error);
+	});
+
+	it("doesn't create workflows for excluded options when specified", async () => {
+		const mockRequest = vi.fn().mockResolvedValue({ status: 200 });
+
+		await initializeBranchProtectionSettings(createMockOctokit(mockRequest), {
+			...stubValues,
+			excludeCompliance: true,
+			excludeLintKnip: true,
+			excludeLintMd: true,
+			excludeLintPackages: true,
+			excludeLintSpelling: true,
+			excludeTests: true,
+		});
+
+		expect(mockRequest.mock.calls).toMatchInlineSnapshot(`
+			[
+			  [
+			    "PUT /repos///branches/main/protection",
+			    {
+			      "allow_deletions": false,
+			      "allow_force_pushes": true,
+			      "allow_fork_pushes": false,
+			      "allow_fork_syncing": true,
+			      "block_creations": false,
+			      "branch": "main",
+			      "enforce_admins": false,
+			      "owner": "",
+			      "repo": "",
+			      "required_conversation_resolution": true,
+			      "required_linear_history": false,
+			      "required_pull_request_reviews": null,
+			      "required_status_checks": {
+			        "checks": [
+			          {
+			            "context": "build",
+			          },
+			          {
+			            "context": "lint",
+			          },
+			          {
+			            "context": "prettier",
+			          },
+			        ],
+			        "strict": false,
+			      },
+			      "restrictions": null,
+			    },
+			  ],
+			]
+		`);
 	});
 });

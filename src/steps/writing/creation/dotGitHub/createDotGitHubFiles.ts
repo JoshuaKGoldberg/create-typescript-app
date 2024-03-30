1a52b29dd34f9893f835b0bf21eebd7adfaef282
Josh Goldberg <git@joshuakgoldberg.com>
2024-03-30 13:10:40 -0400

    fix: ignore codecov-action in renovate.json

--- src/steps/writing/creation/dotGitHub/createDotGitHubFiles.ts
+++ src/steps/writing/creation/dotGitHub/createDotGitHubFiles.ts
@@ -280,6 +280,7 @@ We appreciate your efforts and responsible disclosure and will make every effort
 			"renovate.json": await formatJson({
 				$schema: "https://docs.renovatebot.com/renovate-schema.json",
 				automerge: true,
+				ignoreDeps: ["codecov/codecov-action"],
 				internalChecksFilter: "strict",
 				labels: ["dependencies"],
 				minimumReleaseAge: "3 days",

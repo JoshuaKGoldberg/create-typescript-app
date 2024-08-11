c967173aa97eb8f9064f74f1e29949078550cdbb
Josh Goldberg ✨ <git@joshuakgoldberg.com>
2024-08-10 20:15:38 -0400

    fix: preserve post-badge paragraph in README.md migration (#1609)
    ## PR Checklist
    
    - [x] Addresses an existing open issue: fixes #1159
    - [x] That issue was marked as [`status: accepting
    prs`](https://github.com/JoshuaKGoldberg/create-typescript-app/issues?q=is%3Aopen+is%3Aissue+label%3A%22status%3A+accepting+prs%22)
    - [x] Steps in
    [CONTRIBUTING.md](https://github.com/JoshuaKGoldberg/create-typescript-app/blob/main/.github/CONTRIBUTING.md)
    were taken
    
    ## Overview
    
    Adds a special case for a README.md already having a CTA-style project
    logo image.
    
    💖

--- src/steps/writeReadme/findIntroSectionClose.ts
+++ src/steps/writeReadme/findIntroSectionClose.ts
@@ -1,14 +1,21 @@
 import { existingBadgeMatcherCreators } from "./findExistingBadges.js";
 
 export function findIntroSectionClose(contents: string) {
-	// Highest priority match: an h2, presumably following badges
+	// Highest priority: after an existing create-typescript-app-style logo
+	const projectLogoMatch =
+		/<img align="right" alt="Project logo.+" src=".+">/.exec(contents);
+	if (projectLogoMatch) {
+		return contents.indexOf("\n", projectLogoMatch.index) + 2;
+	}
+
+	// Next: before a first code block or h2, presumably following badges
 	const indexOfH2OrCodeBlock = contents.search(/## |<\s*h2|```/);
 
 	if (indexOfH2OrCodeBlock !== -1) {
 		return indexOfH2OrCodeBlock - 2;
 	}
 
-	// Failing that, if any badges are found, go after the last of them
+	// Failing those, if any badges are found, go after the last of them
 	for (const createMatcher of existingBadgeMatcherCreators) {
 		const lastMatch = [...contents.matchAll(createMatcher())].at(-1);
 

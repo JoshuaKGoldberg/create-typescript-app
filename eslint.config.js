64879eebb0689c20852d5a812eaa3d0f41363833
Josh Goldberg <git@joshuakgoldberg.com>
2024-12-03 13:38:48 -0500

    eslint.config.js normalization

--- eslint.config.js
+++ eslint.config.js
@@ -108,17 +108,11 @@ export default tseslint.config(
 			"yml/file-extension": ["error", { extension: "yml" }],
 			"yml/sort-keys": [
 				"error",
-				{
-					order: { type: "asc" },
-					pathPattern: "^.*$",
-				},
+				{ order: { type: "asc" }, pathPattern: "^.*$" },
 			],
 			"yml/sort-sequence-values": [
 				"error",
-				{
-					order: { type: "asc" },
-					pathPattern: "^.*$",
-				},
+				{ order: { type: "asc" }, pathPattern: "^.*$" },
 			],
 		},
 	},

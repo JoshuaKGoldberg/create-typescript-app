c762de964518ee2bcd87c98d8f9054e0aec7b20d
Josh Goldberg <git@joshuakgoldberg.com>
2024-12-05 12:49:20 -0500

    Also reset src/index.ts

--- src/steps/removeSetupScripts.ts
+++ src/steps/removeSetupScripts.ts
@@ -1,5 +1,7 @@
 import * as fs from "node:fs/promises";
 
+import { formatTypeScript } from "./writing/creation/formatters/formatTypeScript.js";
+
 const globPaths = [
 	"./bin",
 	"./docs",
@@ -17,4 +19,11 @@ export async function removeSetupScripts() {
 	for (const globPath of globPaths) {
 		await fs.rm(globPath, { force: true, recursive: true });
 	}
+
+	await fs.writeFile(
+		"./src/index.ts",
+		await formatTypeScript(
+			[`export * from "./greet.js";`, `export * from "./types.js";`].join("\n"),
+		),
+	);
 }

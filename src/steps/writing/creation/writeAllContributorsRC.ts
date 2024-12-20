a4257e5950d42898535c5997d9fca9ab39e7c670
Josh Goldberg ✨ <git@joshuakgoldberg.com>
2024-12-20 17:23:06 -0500

    fix: remove unnecessary default values from .all-contributorsrc (#1784)
    ## PR Checklist
    
    - [x] Addresses an existing open issue: fixes #1783
    - [x] That issue was marked as [`status: accepting
    prs`](https://github.com/JoshuaKGoldberg/create-typescript-app/issues?q=is%3Aopen+is%3Aissue+label%3A%22status%3A+accepting+prs%22)
    - [x] Steps in
    [CONTRIBUTING.md](https://github.com/JoshuaKGoldberg/create-typescript-app/blob/main/.github/CONTRIBUTING.md)
    were taken
    
    ## Overview
    
    Straightforward removal of config settings. Note the lack of actual
    changes to the `README.md` file.
    
    🔪

--- src/steps/writing/creation/writeAllContributorsRC.ts
+++ src/steps/writing/creation/writeAllContributorsRC.ts
@@ -10,17 +10,9 @@ export async function writeAllContributorsRC(options: Options) {
 	return await formatJson({
 		badgeTemplate:
 			'	<a href="#contributors" target="_blank"><img alt="👪 All Contributors: <%= contributors.length %>" src="https://img.shields.io/badge/%F0%9F%91%AA_all_contributors-<%= contributors.length %>-21bb42.svg" /></a>',
-		commit: false,
-		commitConvention: "angular",
-		commitType: "docs",
 		contributors: existing?.contributors ?? [],
-		contributorsPerLine: 7,
 		contributorsSortAlphabetically: true,
-		files: ["README.md"],
-		imageSize: 100,
 		projectName: options.repository,
 		projectOwner: options.owner,
-		repoHost: "https://github.com",
-		repoType: "github",
 	});
 }

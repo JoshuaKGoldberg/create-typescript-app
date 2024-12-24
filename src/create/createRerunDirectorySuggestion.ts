1fea957cce6fa79229c65be7120b981186c0e997
Josh Goldberg ✨ <git@joshuakgoldberg.com>
2024-12-24 13:02:25 -0500

    Apply suggestions from code review

--- src/create/createRerunDirectorySuggestion.ts
+++ src/create/createRerunDirectorySuggestion.ts
@@ -1,10 +1,6 @@
 import { Options } from "../shared/types.js";
 
 export function createRerunDirectorySuggestion(options: Partial<Options>) {
-	if (!options.directory) {
-		return undefined;
-	}
-
 	const defaultValue = options.mode === "create" ? options.repository : ".";
 
 	return options.directory === defaultValue ? undefined : options.directory;

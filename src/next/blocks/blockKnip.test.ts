f92166adaf18c313600168cefc265b7ab8c702e1
Josh Goldberg ✨ <git@joshuakgoldberg.com>
2025-01-14 19:38:19 -0500

    feat: add addons.ignoreDependencies to blockKnip (#1863)
    ## PR Checklist
    
    - [x] Addresses an existing open issue: fixes #1851
    - [x] That issue was marked as [`status: accepting
    prs`](https://github.com/JoshuaKGoldberg/create-typescript-app/issues?q=is%3Aopen+is%3Aissue+label%3A%22status%3A+accepting+prs%22)
    - [x] Steps in
    [CONTRIBUTING.md](https://github.com/JoshuaKGoldberg/create-typescript-app/blob/main/.github/CONTRIBUTING.md)
    were taken
    
    ## Overview
    
    💖

---
+++ src/next/blocks/blockKnip.test.ts
@@ -0,0 +1,130 @@
+import { testBlock } from "create-testers";
+import { describe, expect, test, vi } from "vitest";
+
+import { blockKnip } from "./blockKnip.js";
+import { optionsBase } from "./options.fakes.js";
+
+vi.mock("../utils/resolveBin.js", () => ({
+	resolveBin: (bin: string) => `path/to/${bin}`,
+}));
+
+describe("blockKnip", () => {
+	test("without addons", () => {
+		const creation = testBlock(blockKnip, {
+			options: optionsBase,
+		});
+
+		expect(creation).toMatchInlineSnapshot(`
+			{
+			  "addons": [
+			    {
+			      "addons": {
+			        "sections": {
+			          "Linting": {
+			            "contents": {
+			              "items": [
+			                "- \`pnpm lint:knip\` ([knip](https://github.com/webpro/knip)): Detects unused files, dependencies, and code exports",
+			              ],
+			            },
+			          },
+			        },
+			      },
+			      "block": [Function],
+			    },
+			    {
+			      "addons": {
+			        "jobs": [
+			          {
+			            "name": "Lint Knip",
+			            "steps": [
+			              {
+			                "run": "pnpm lint:knip",
+			              },
+			            ],
+			          },
+			        ],
+			      },
+			      "block": [Function],
+			    },
+			    {
+			      "addons": {
+			        "properties": {
+			          "devDependencies": {
+			            "knip": "5.41.1",
+			          },
+			          "scripts": {
+			            "lint:knip": "knip",
+			          },
+			        },
+			      },
+			      "block": [Function],
+			    },
+			  ],
+			  "files": {
+			    "knip.json": "{"$schema":"https://unpkg.com/knip@5.41.1/schema.json","entry":["src/index.ts"],"ignoreExportsUsedInFile":{"interface":true,"type":true},"project":["src/**/*.ts"]}",
+			  },
+			}
+		`);
+	});
+
+	test("with addons", () => {
+		const creation = testBlock(blockKnip, {
+			addons: {
+				ignoreDependencies: ["abc", "def"],
+			},
+			options: optionsBase,
+		});
+
+		expect(creation).toMatchInlineSnapshot(`
+			{
+			  "addons": [
+			    {
+			      "addons": {
+			        "sections": {
+			          "Linting": {
+			            "contents": {
+			              "items": [
+			                "- \`pnpm lint:knip\` ([knip](https://github.com/webpro/knip)): Detects unused files, dependencies, and code exports",
+			              ],
+			            },
+			          },
+			        },
+			      },
+			      "block": [Function],
+			    },
+			    {
+			      "addons": {
+			        "jobs": [
+			          {
+			            "name": "Lint Knip",
+			            "steps": [
+			              {
+			                "run": "pnpm lint:knip",
+			              },
+			            ],
+			          },
+			        ],
+			      },
+			      "block": [Function],
+			    },
+			    {
+			      "addons": {
+			        "properties": {
+			          "devDependencies": {
+			            "knip": "5.41.1",
+			          },
+			          "scripts": {
+			            "lint:knip": "knip",
+			          },
+			        },
+			      },
+			      "block": [Function],
+			    },
+			  ],
+			  "files": {
+			    "knip.json": "{"$schema":"https://unpkg.com/knip@5.41.1/schema.json","entry":["src/index.ts"],"ignoreDependencies":["abc","def"],"ignoreExportsUsedInFile":{"interface":true,"type":true},"project":["src/**/*.ts"]}",
+			  },
+			}
+		`);
+	});
+});

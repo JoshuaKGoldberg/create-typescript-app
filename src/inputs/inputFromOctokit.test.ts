c04bb29b22bd3cd240e5ec00174b3df28044683e
Josh Goldberg ✨ <git@joshuakgoldberg.com>
2025-03-04 11:18:06 -0500

    fix: don't crash if GitHub API call for rulesetId option fails (#1953)
    ## PR Checklist
    
    - [x] Addresses an existing open issue: fixes #1952
    - [x] That issue was marked as [`status: accepting
    prs`](https://github.com/JoshuaKGoldberg/create-typescript-app/issues?q=is%3Aopen+is%3Aissue+label%3A%22status%3A+accepting+prs%22)
    - [x] Steps in
    [CONTRIBUTING.md](https://github.com/JoshuaKGoldberg/create-typescript-app/blob/main/.github/CONTRIBUTING.md)
    were taken
    
    ## Overview
    
    A nice exercise of the input testers.
    
    🎁

---
+++ src/inputs/inputFromOctokit.test.ts
@@ -0,0 +1,45 @@
+import { createMockFetchers, testInput } from "bingo-testers";
+import { describe, expect, it, vi } from "vitest";
+
+import { inputFromOctokit } from "./inputFromOctokit.js";
+
+describe("inputFromOctokit", () => {
+	it("returns data when the request resolves", async () => {
+		const data = JSON.stringify({ found: true });
+
+		const actual = await testInput(inputFromOctokit, {
+			args: {
+				endpoint: "GET /repos/{owner}/{repo}/rulesets",
+				options: {},
+			},
+			fetchers: createMockFetchers(
+				vi.fn().mockResolvedValueOnce(
+					// eslint-disable-next-line n/no-unsupported-features/node-builtins
+					new Response(data),
+				),
+			),
+		});
+
+		expect(actual).toEqual(data);
+	});
+
+	it("returns undefined when the request rejects", async () => {
+		const actual = await testInput(inputFromOctokit, {
+			args: {
+				endpoint: "GET /repos/{owner}/{repo}/rulesets",
+				options: {},
+			},
+			fetchers: createMockFetchers(
+				vi.fn().mockResolvedValueOnce(
+					// eslint-disable-next-line n/no-unsupported-features/node-builtins
+					new Response("", {
+						status: 404,
+						statusText: "Not found.",
+					}),
+				),
+			),
+		});
+
+		expect(actual).toBe(undefined);
+	});
+});

99cc5f157e6608b2b87237985a736d3957317f3b
Josh Goldberg <git@joshuakgoldberg.com>
2025-03-05 18:42:18 -0500

    feat: release 2.0-stable

--- src/integration.test.ts
+++ src/integration.test.ts
@@ -40,67 +40,69 @@ test("Producing the everything preset matches the files in this repository", asy
 	})) as IntakeDirectory;
 
 	const created = producePreset(presets.everything, {
-		addons: [
-			blockCodecov({
-				env: {
-					CODECOV_TOKEN: "${{ secrets.CODECOV_TOKEN }}",
-				},
-			}),
-			blockCSpell({
-				words: [
-					"Anson",
-					"apexskier",
-					"dbaeumer",
-					"joshuakgoldberg",
-					"markdownlintignore",
-					"mtfoley",
-					"infile",
-					"npmjs",
-				],
-			}),
-			blockESLint({
-				explanations: [
-					`👋 Hi! This ESLint configuration contains a lot more stuff than many repos'!
+		options: (await prepareOptions(base)) as BaseOptions,
+		refinements: {
+			addons: [
+				blockCodecov({
+					env: {
+						CODECOV_TOKEN: "${{ secrets.CODECOV_TOKEN }}",
+					},
+				}),
+				blockCSpell({
+					words: [
+						"Anson",
+						"apexskier",
+						"dbaeumer",
+						"joshuakgoldberg",
+						"markdownlintignore",
+						"mtfoley",
+						"infile",
+						"npmjs",
+					],
+				}),
+				blockESLint({
+					explanations: [
+						`👋 Hi! This ESLint configuration contains a lot more stuff than many repos'!
 You can read from it to see all sorts of linting goodness, but don't worry -
 it's not something you need to exhaustively understand immediately. 💙
 
 If you're interested in learning more, see the 'getting started' docs on:
 - ESLint: https://eslint.org
 - typescript-eslint: https://typescript-eslint.io`,
-				],
-				rules: [
-					{
-						comment:
-							"These on-by-default rules work well for this repo if configured",
-						entries: {
-							"@typescript-eslint/prefer-nullish-coalescing": [
-								"error",
-								{ ignorePrimitives: true },
-							],
-							"@typescript-eslint/restrict-template-expressions": [
-								"error",
-								{ allowBoolean: true, allowNullish: true, allowNumber: true },
-							],
+					],
+					rules: [
+						{
+							comment:
+								"These on-by-default rules work well for this repo if configured",
+							entries: {
+								"@typescript-eslint/prefer-nullish-coalescing": [
+									"error",
+									{ ignorePrimitives: true },
+								],
+								"@typescript-eslint/restrict-template-expressions": [
+									"error",
+									{ allowBoolean: true, allowNullish: true, allowNumber: true },
+								],
+							},
 						},
-					},
-				],
-			}),
-			blockKnip({
-				ignoreDependencies: [
-					"all-contributors-cli",
-					"cspell-populate-words",
-					"remove-dependencies",
-				],
-			}),
-			blockTSup({
-				runArgs: ["--version"],
-			}),
-		],
-		blocks: {
-			add: [blockAreTheTypesWrong],
-			exclude: [blockTemplatedWith],
+					],
+				}),
+				blockKnip({
+					ignoreDependencies: [
+						"all-contributors-cli",
+						"cspell-populate-words",
+						"remove-dependencies",
+					],
+				}),
+				blockTSup({
+					runArgs: ["--version"],
+				}),
+			],
+			blocks: {
+				add: [blockAreTheTypesWrong],
+				exclude: [blockTemplatedWith],
+			},
 		},
-		options: (await prepareOptions(base)) as BaseOptions,
 	});
 
 	const processText = (text: string, filePath: string) =>
@@ -108,32 +110,7 @@ If you're interested in learning more, see the 'getting started' docs on:
 			? prettier.format(text, { filepath: filePath, useTabs: true })
 			: text;
 
-	// Right now, there is exactly one change: the altered beta release flow
-	// TODO: That will be removed once releases switch back to stable:
-	// https://github.com/JoshuaKGoldberg/create-typescript-app/issues/1831
-	expect(diffCreatedDirectory(actual, created.files, processText))
-		.toMatchInlineSnapshot(`
-			{
-			  ".github": {
-			    "workflows": {
-			      "release.yml": "@@ -14,13 +14,9 @@
-			       - run: pnpm build
-			       - env:
-			           GITHUB_TOKEN: \${{ secrets.ACCESS_TOKEN }}
-			           NPM_TOKEN: \${{ secrets.NPM_TOKEN }}
-			-        run: |
-			-          git config --global user.email "git@joshuakgoldberg.com"
-			-          git config --global user.name "Josh Goldberg"
-			-          npm config set //registry.npmjs.org/:_authToken $NPM_TOKEN
-			-      - run: npx release-it --preRelease=beta
-			+        uses: JoshuaKGoldberg/release-it-action@v0.2.2
-			 
-			 name: Release
-			 
-			 on:
-			",
-			    },
-			  },
-			}
-		`);
+	expect(
+		diffCreatedDirectory(actual, created.files, processText),
+	).toBeUndefined();
 });

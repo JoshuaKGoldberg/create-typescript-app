9a95837b4130ffd8e7e71d0469b158a48715e0fd
Josh Goldberg <git@joshuakgoldberg.com>
2025-03-21 16:09:20 -0400

    Can't use a snapshot because package version

--- src/blocks/blockCTATransitions.test.ts
+++ src/blocks/blockCTATransitions.test.ts
@@ -1,7 +1,10 @@
 import { testBlock } from "bingo-stratum-testers";
 import { describe, expect, test } from "vitest";
 
+import { packageData } from "../data/packageData.js";
 import { blockCTATransitions } from "./blockCTATransitions.js";
+import { blockGitHubActionsCI } from "./blockGitHubActionsCI.js";
+import { blockPackageJson } from "./blockPackageJson.js";
 import { optionsBase } from "./options.fakes.js";
 
 describe("blockCTATransitions", () => {
@@ -10,66 +13,67 @@ describe("blockCTATransitions", () => {
 			options: optionsBase,
 		});
 
-		expect(creation).toMatchInlineSnapshot(`
-			{
-			  "addons": [
-			    {
-			      "addons": {
-			        "jobs": [
-			          {
-			            "checkoutWith": {
-			              "fetch-depth": "0",
-			              "ref": "\${{github.event.pull_request.head.ref}}",
-			              "repository": "\${{github.event.pull_request.head.repo.full_name}}",
-			              "token": ""\${{ secrets.ACCESS_TOKEN }}"",
-			            },
-			            "if": "\${{ startsWith(github.head_ref, 'renovate/') && contains(github.event.pull_request.title, 'create-typescript-app') }}",
-			            "name": "CTA Transitions",
-			            "steps": [
-			              {
-			                "run": "pnpx create-typescript-app",
-			              },
-			              {
-			                "uses": "stefanzweifel/git-auto-commit-action@v5",
-			                "with": {
-			                  "commit_author": "The Friendly Bingo Bot <bot@create.bingo>",
-			                  "commit_message": "Check in changes from re-running npx create-typescript-app",
-			                  "commit_user_email": "bot@create.bingo",
-			                  "commit_user_name": "The Friendly Bingo Bot",
-			                },
-			              },
-			              {
-			                "uses": "mshick/add-pr-comment@v2",
-			                "with": {
-			                  "issue": "\${{ github.event.pull_request.number }}",
-			                  "message": "|
-			🤖 Beep boop! I ran \`npx create-typescript-app\` and found same changes.
-			Please review the latest commit to see if you want to merge it.
-			Cheers! 💝
+		expect(creation).toEqual({
+			addons: [
+				{
+					addons: {
+						jobs: [
+							{
+								checkoutWith: {
+									"fetch-depth": "0",
+									ref: "${{github.event.pull_request.head.ref}}",
+									repository:
+										"${{github.event.pull_request.head.repo.full_name}}",
+									token: '"${{ secrets.ACCESS_TOKEN }}"',
+								},
+								if: "${{ startsWith(github.head_ref, 'renovate/') && contains(github.event.pull_request.title, 'create-typescript-app') }}",
+								name: "CTA Transitions",
+								steps: [
+									{
+										run: "pnpx create-typescript-app",
+									},
+									{
+										uses: "stefanzweifel/git-auto-commit-action@v5",
+										with: {
+											commit_author:
+												"The Friendly Bingo Bot <bot@create.bingo>",
+											commit_message:
+												"Check in changes from re-running npx create-typescript-app",
+											commit_user_email: "bot@create.bingo",
+											commit_user_name: "The Friendly Bingo Bot",
+										},
+									},
+									{
+										uses: "mshick/add-pr-comment@v2",
+										with: {
+											issue: "${{ github.event.pull_request.number }}",
+											message: `|
+🤖 Beep boop! I ran \`npx create-typescript-app\` and found same changes.
+Please review the latest commit to see if you want to merge it.
+Cheers! 💝
 
-			> This change was automatically made in CI to keep your repository up-to-date with the templating in [create-typescript-app](https://github.com/JoshuaKGoldberg/create-typescript-app).
-			> If you want to opt out of these automatic updates, delete the \`.github/workflows/cta-transitions.yml\` file on your \`main\` branch.",
-			                  "repo-token": "\${{ secrets.ACCESS_TOKEN }}",
-			                },
-			              },
-			            ],
-			          },
-			        ],
-			      },
-			      "block": [Function],
-			    },
-			    {
-			      "addons": {
-			        "properties": {
-			          "devDependencies": {
-			            "create-typescript-app": "2.8.0",
-			          },
-			        },
-			      },
-			      "block": [Function],
-			    },
-			  ],
-			}
-		`);
+> This change was automatically made in CI to keep your repository up-to-date with the templating in [create-typescript-app](https://github.com/JoshuaKGoldberg/create-typescript-app).
+> If you want to opt out of these automatic updates, delete the \`.github/workflows/cta-transitions.yml\` file on your \`main\` branch.`,
+											"repo-token": "${{ secrets.ACCESS_TOKEN }}",
+										},
+									},
+								],
+							},
+						],
+					},
+					block: blockGitHubActionsCI,
+				},
+				{
+					addons: {
+						properties: {
+							devDependencies: {
+								"create-typescript-app": packageData.version,
+							},
+						},
+					},
+					block: blockPackageJson,
+				},
+			],
+		});
 	});
 });

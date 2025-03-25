dcfa47bbfd38a1d0f30cc8fb493bd0d1fc570735
Josh Goldberg ✨ <git@joshuakgoldberg.com>
2025-03-25 15:40:07 -0400

    feat: standalone CTA Transitions workflow (#2012)
    <!-- 👋 Hi, thanks for sending a PR to create-typescript-app! 🎁.
    Please fill out all fields below and make sure each item is true and [x]
    checked.
    Otherwise we may not be able to review your PR. -->
    
    ## PR Checklist
    
    - [x] Addresses an existing open issue: fixes #2009
    - [x] That issue was marked as [`status: accepting
    prs`](https://github.com/JoshuaKGoldberg/create-typescript-app/issues?q=is%3Aopen+is%3Aissue+label%3A%22status%3A+accepting+prs%22)
    - [x] Steps in
    [CONTRIBUTING.md](https://github.com/JoshuaKGoldberg/create-typescript-app/blob/main/.github/CONTRIBUTING.md)
    were taken
    
    ## Overview
    
    Moves the transition mode logic into two files:
    
    1. `.github/actions/transition/action.yml`: A dedicated composite action
    for running in transition mode and checking in & commenting any changes
    2. `.github/workflows/cta.yml`: Checks if the action should be run, and
    runs it if so
    
    This means the job won't be marked as _Skipped_ if the logic is skipped.
    It'll still be _Success_ and thus not block PRs.
    
    🎁

--- src/blocks/blockCTATransitions.ts
+++ src/blocks/blockCTATransitions.ts
@@ -1,8 +1,10 @@
 import { base } from "../base.js";
 import { packageData } from "../data/packageData.js";
 import { resolveUses } from "./actions/resolveUses.js";
-import { blockGitHubActionsCI } from "./blockGitHubActionsCI.js";
 import { blockPackageJson } from "./blockPackageJson.js";
+import { blockRepositoryBranchRuleset } from "./blockRepositoryBranchRuleset.js";
+import { createSoloWorkflowFile } from "./files/createSoloWorkflowFile.js";
+import { formatYamlAction } from "./files/formatYamlAction.js";
 
 export const blockCTATransitions = base.createBlock({
 	about: {
@@ -11,58 +13,6 @@ export const blockCTATransitions = base.createBlock({
 	produce({ options }) {
 		return {
 			addons: [
-				blockGitHubActionsCI({
-					jobs: [
-						{
-							checkoutWith: {
-								"fetch-depth": "0",
-								ref: "${{github.event.pull_request.head.ref}}",
-								repository:
-									"${{github.event.pull_request.head.repo.full_name}}",
-								token: '"${{ secrets.ACCESS_TOKEN }}"',
-							},
-							if: "${{ startsWith(github.head_ref, 'renovate/') && contains(github.event.pull_request.title, 'create-typescript-app') }}",
-							name: "CTA Transitions",
-							steps: [
-								{ run: "pnpx create-typescript-app" },
-								{
-									uses: resolveUses(
-										"stefanzweifel/git-auto-commit-action",
-										"v5",
-										options.workflowsVersions,
-									),
-									with: {
-										commit_author: "The Friendly Bingo Bot <bot@create.bingo>",
-										commit_message:
-											"Check in changes from re-running npx create-typescript-app",
-										commit_user_email: "bot@create.bingo",
-										commit_user_name: "The Friendly Bingo Bot",
-									},
-								},
-								{
-									uses: resolveUses(
-										"mshick/add-pr-comment",
-										"v2",
-										options.workflowsVersions,
-									),
-									with: {
-										issue: "${{ github.event.pull_request.number }}",
-										message: [
-											"|",
-											"🤖 Beep boop! I ran `npx create-typescript-app` and found same changes.",
-											"Please review the latest commit to see if you want to merge it.",
-											"Cheers! 💝",
-											"",
-											"> This change was automatically made in CI to keep your repository up-to-date with the templating in [create-typescript-app](https://github.com/JoshuaKGoldberg/create-typescript-app).",
-											"> If you want to opt out of these automatic updates, delete the `.github/workflows/cta-transitions.yml` file on your `main` branch.",
-										].join("\n"),
-										"repo-token": "${{ secrets.ACCESS_TOKEN }}",
-									},
-								},
-							],
-						},
-					],
-				}),
 				blockPackageJson({
 					properties: {
 						devDependencies: {
@@ -70,7 +20,98 @@ export const blockCTATransitions = base.createBlock({
 						},
 					},
 				}),
+				blockRepositoryBranchRuleset({
+					requiredStatusChecks: ["Transition"],
+				}),
 			],
+			files: {
+				".github": {
+					actions: {
+						transition: {
+							"action.yml": formatYamlAction({
+								description: "Runs create-typescript-app in transition mode",
+								name: "Transition",
+								runs: {
+									steps: [
+										{ uses: "./.github/actions/prepare" },
+										{
+											run: "pnpx create-typescript-app",
+											shell: "bash",
+										},
+										{
+											id: "auto-commit-action",
+											uses: "stefanzweifel/git-auto-commit-action@v5",
+											with: {
+												commit_author:
+													"The Friendly Bingo Bot <bot@create.bingo>",
+												commit_message:
+													"Check in changes from re-running npx create-typescript-app",
+												commit_user_email: "bot@create.bingo",
+												commit_user_name: "The Friendly Bingo Bot",
+											},
+										},
+										{
+											if: "steps.auto-commit-action.outputs.changes_detected == 'true'",
+											uses: "mshick/add-pr-comment@v2",
+											with: {
+												issue: "${{ github.event.pull_request.number }}",
+												message: [
+													"🤖 Beep boop! I ran `pnpx create-typescript-app` and it updated some files.",
+													"I went ahead and checked those changes into this PR for you. Please review the latest commit to see if you want to merge it.",
+													"Cheers!",
+													" — _The Friendly Bingo Bot_ 💝",
+													"",
+													"> ℹ️ These automatic commits keep your repository up-to-date with new versions of [create-typescript-app](https://github.com/JoshuaKGoldberg/create-typescript-app). If you want to opt out, delete your `.github/workflows/cta-transitions.yml` file.",
+												].join("\n"),
+											},
+										},
+									],
+									using: "composite",
+								},
+							}),
+						},
+					},
+					workflows: {
+						"cta.yml": createSoloWorkflowFile({
+							jobName: "Transition",
+							name: "CTA",
+							on: {
+								pull_request: {
+									branches: ["main"],
+								},
+							},
+							permissions: {
+								"pull-requests": "write",
+							},
+							steps: [
+								{
+									uses: resolveUses(
+										"actions/checkout",
+										"v4",
+										options.workflowsVersions,
+									),
+									with: {
+										"fetch-depth": 0,
+										ref: "${{github.event.pull_request.head.ref}}",
+										repository:
+											"${{github.event.pull_request.head.repo.full_name}}",
+										token: "${{ secrets.ACCESS_TOKEN }}",
+									},
+								},
+								{
+									id: "check",
+									if: `(github.actor == '${options.owner}' || github.actor == 'renovate[bot]') && startsWith(github.head_ref, 'renovate/') && contains(github.event.pull_request.title, 'create-typescript-app')`,
+									uses: "./.github/actions/transition",
+								},
+								{
+									if: "steps.check.outcome == 'skipped'",
+									run: "echo 'Skipping transition mode because the PR does not appear to be an automated or owner-created update to create-typescript-app.'",
+								},
+							],
+						}),
+					},
+				},
+			},
 		};
 	},
 });

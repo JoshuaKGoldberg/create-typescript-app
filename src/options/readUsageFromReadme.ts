bd9c686cdf99e5781713721da6c585673acf46f6
Josh Goldberg <git@joshuakgoldberg.com>
2025-03-27 10:40:16 -0400

    fix: more permissive usage bounds and headings

--- src/options/readUsageFromReadme.ts
+++ src/options/readUsageFromReadme.ts
@@ -1,4 +1,3 @@
-const startDevelopment = "## Development";
 const startUsage = "## Usage";
 
 export function readUsageFromReadme(readme: string) {
@@ -7,17 +6,15 @@ export function readUsageFromReadme(readme: string) {
 		return undefined;
 	}
 
-	const indexOfDevelopment = readme.indexOf(
-		startDevelopment,
-		indexOfUsage + startUsage.length,
-	);
-	if (indexOfDevelopment === -1) {
-		return undefined;
+	const offset = indexOfUsage + startUsage.length;
+	const indexOfNextKnownHeading = readme
+		.slice(offset)
+		.search(/## (?:Development|Contributing|Contributors)/);
+	if (indexOfNextKnownHeading === -1) {
+		return readme.slice(offset);
 	}
 
-	const usage = readme
-		.slice(indexOfUsage + startUsage.length, indexOfDevelopment)
-		.trim();
+	const usage = readme.slice(offset, indexOfNextKnownHeading + offset).trim();
 
 	return usage ? usage : undefined;
 }

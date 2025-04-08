0a62e70ba28ec6349bb056e1b7c9096d8a1bba80
Josh Goldberg ✨ <git@joshuakgoldberg.com>
2025-04-08 16:22:18 -0500

    feat: add build --no-dts before eslint-doc-generator in blockESLintPlugin build:docs (#2186)
    ## PR Checklist
    
    - [x] Addresses an existing open issue: fixes #2182
    - [x] That issue was marked as [`status: accepting
    prs`](https://github.com/JoshuaKGoldberg/create-typescript-app/issues?q=is%3Aopen+is%3Aissue+label%3A%22status%3A+accepting+prs%22)
    - [x] Steps in
    [CONTRIBUTING.md](https://github.com/JoshuaKGoldberg/create-typescript-app/blob/main/.github/CONTRIBUTING.md)
    were taken
    
    ## Overview
    
    🎁

--- src/blocks/blockESLintPlugin.ts
+++ src/blocks/blockESLintPlugin.ts
@@ -74,7 +74,7 @@ pnpm build:docs
 							"eslint-plugin-eslint-plugin": "6.4.0",
 						},
 						scripts: {
-							"build:docs": "eslint-doc-generator",
+							"build:docs": "pnpm build --no-dts && eslint-doc-generator",
 							"lint:docs": "eslint-doc-generator --check",
 						},
 					},

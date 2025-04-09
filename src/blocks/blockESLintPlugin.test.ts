547974c067bda248b602365e97e64f407391f6e9
Josh Goldberg <git@joshuakgoldberg.com>
2025-04-09 09:25:31 -0400

    Switch to AST parsing strategy to handle functions

--- src/blocks/blockESLintPlugin.test.ts
+++ src/blocks/blockESLintPlugin.test.ts
@@ -1,11 +1,11 @@
-import { testBlock } from "bingo-stratum-testers";
-import { describe, expect, test } from "vitest";
+import { testBlock, testIntake } from "bingo-stratum-testers";
+import { describe, expect, it, test } from "vitest";
 
 import { blockESLintPlugin } from "./blockESLintPlugin.js";
 import { optionsBase } from "./options.fakes.js";
 
 describe("blockESLintPlugin", () => {
-	test("without options or mode", () => {
+	test("without addons, mode, or options", () => {
 		const creation = testBlock(blockESLintPlugin, {
 			options: optionsBase,
 		});
@@ -505,5 +505,266 @@ describe("blockESLintPlugin", () => {
 			  },
 			}
 		`);
+	});
+
+	test("addons", () => {
+		const creation = testBlock(blockESLintPlugin, {
+			addons: {
+				configEmoji: [
+					["recommended", "✅"],
+					["legacy-recommended", "✔️"],
+				],
+			},
+			options: optionsBase,
+		});
+
+		expect(creation).toMatchInlineSnapshot(`
+			{
+			  "addons": [
+			    {
+			      "addons": {
+			        "words": [
+			          "eslint-doc-generatorrc",
+			        ],
+			      },
+			      "block": [Function],
+			    },
+			    {
+			      "addons": {
+			        "sections": {
+			          "Building": {
+			            "innerSections": [
+			              {
+			                "contents": "
+			Run [\`eslint-doc-generator\`](https://github.com/bmish/eslint-doc-generator) to generate Markdown files documenting rules.
+
+			\`\`\`shell
+			pnpm build:docs
+			\`\`\`
+					",
+			                "heading": "Building Docs",
+			              },
+			            ],
+			          },
+			          "Linting": {
+			            "contents": {
+			              "items": [
+			                "- \`pnpm lint:docs\` ([eslint-doc-generator](https://github.com/bmish/eslint-doc-generator)): Generates and validates documentation for ESLint rules",
+			              ],
+			            },
+			          },
+			        },
+			      },
+			      "block": [Function],
+			    },
+			    {
+			      "addons": {
+			        "extensions": [
+			          "eslintPlugin.configs["flat/recommended"]",
+			        ],
+			        "ignores": [
+			          ".eslint-doc-generatorrc.js",
+			          "docs/rules/*/*.ts",
+			        ],
+			        "imports": [
+			          {
+			            "source": {
+			              "packageName": "eslint-plugin-eslint-plugin",
+			              "version": "6.4.0",
+			            },
+			            "specifier": "eslintPlugin",
+			          },
+			        ],
+			      },
+			      "block": [Function],
+			    },
+			    {
+			      "addons": {
+			        "jobs": [
+			          {
+			            "name": "Lint Docs",
+			            "steps": [
+			              {
+			                "run": "pnpm build || exit 0",
+			              },
+			              {
+			                "run": "pnpm lint:docs",
+			              },
+			            ],
+			          },
+			        ],
+			      },
+			      "block": [Function],
+			    },
+			    {
+			      "addons": {
+			        "properties": {
+			          "devDependencies": {
+			            "eslint-doc-generator": "2.1.0",
+			            "eslint-plugin-eslint-plugin": "6.4.0",
+			          },
+			          "scripts": {
+			            "build:docs": "pnpm build --no-dts && eslint-doc-generator",
+			            "lint:docs": "eslint-doc-generator --check",
+			          },
+			        },
+			      },
+			      "block": [Function],
+			    },
+			    {
+			      "addons": {
+			        "coverage": {
+			          "exclude": [
+			            "src/index.ts",
+			            "src/rules/index.ts",
+			          ],
+			        },
+			      },
+			      "block": [Function],
+			    },
+			  ],
+			  "files": {
+			    ".eslint-doc-generatorrc.js": "import prettier from "prettier";
+
+			/** @type {import('eslint-doc-generator').GenerateOptions} */
+			const config = {
+				configEmoji: [["recommended","✅"],["legacy-recommended","✔️"]],
+				postprocess: async (content, path) =>
+					prettier.format(content, {
+						...(await prettier.resolveConfig(path)),
+						parser: "markdown",
+					}),
+				ruleDocTitleFormat: "name",
+			};
+
+			export default config;
+			",
+			  },
+			}
+		`);
+	});
+
+	describe("intake", () => {
+		it("returns nothing when .eslint-doc-generatorrc.js and .eslint-doc-generatorrc.mjs do not exist", () => {
+			const actual = testIntake(blockESLintPlugin, {
+				files: {},
+				options: optionsBase,
+			});
+
+			expect(actual).toEqual(undefined);
+		});
+
+		it("returns nothing when .eslint-doc-generatorrc.js passes nothing to config =", () => {
+			const actual = testIntake(blockESLintPlugin, {
+				files: {
+					".eslint-doc-generatorrc.js": [`const config = {};`],
+				},
+				options: optionsBase,
+			});
+
+			expect(actual).toEqual(undefined);
+		});
+
+		it("returns nothing when .eslint-doc-generatorrc.js passes invalid syntax to config =", () => {
+			const actual = testIntake(blockESLintPlugin, {
+				files: {
+					".eslint-doc-generatorrc.js": [`const config = { ! }`],
+				},
+				options: optionsBase,
+			});
+
+			expect(actual).toEqual(undefined);
+		});
+
+		it("returns nothing when .eslint-doc-generatorrc.js passes unrelated properties to config =", () => {
+			const actual = testIntake(blockESLintPlugin, {
+				files: {
+					".eslint-doc-generatorrc.js": [`const config = { other: true }`],
+				},
+				options: optionsBase,
+			});
+
+			expect(actual).toEqual(undefined);
+		});
+
+		it("returns configEmoji when it exists alone in .eslint-doc-generatorrc.js", () => {
+			const actual = testIntake(blockESLintPlugin, {
+				files: {
+					".eslint-doc-generatorrc.js": [
+						`const config = { configEmoji: [["recommended", "✅"]] }`,
+					],
+				},
+				options: optionsBase,
+			});
+
+			expect(actual).toEqual({
+				configEmoji: [["recommended", "✅"]],
+			});
+		});
+	});
+
+	it("returns configEmoji when it exists alone in .eslint-doc-generatorrc.mjs", () => {
+		const actual = testIntake(blockESLintPlugin, {
+			files: {
+				".eslint-doc-generatorrc.mjs": [
+					`const config = { configEmoji: [["recommended", "✅"]] }`,
+				],
+			},
+			options: optionsBase,
+		});
+
+		expect(actual).toEqual({
+			configEmoji: [["recommended", "✅"]],
+		});
+	});
+
+	it("returns configEmoji when it exists with other data in .eslint-doc-generatorrc.js", () => {
+		const actual = testIntake(blockESLintPlugin, {
+			files: {
+				".eslint-doc-generatorrc.js": [
+					`const config = { configEmoji: [["recommended", "✅"]], other: true }`,
+				],
+			},
+			options: optionsBase,
+		});
+
+		expect(actual).toEqual({
+			configEmoji: [["recommended", "✅"]],
+		});
+	});
+
+	it("returns configEmoji when it exists with other, non-JSON5 data in a full .eslint-doc-generatorrc.js", () => {
+		const actual = testIntake(blockESLintPlugin, {
+			files: {
+				".eslint-doc-generatorrc.js": [
+					`import prettier from "prettier";
+
+/** @type {import('eslint-doc-generator').GenerateOptions} */
+const config = {
+	configEmoji: [
+		["recommended", "✅"],
+		["legacy-recommended", "✔️"],
+	],
+	postprocess: async (content, path) =>
+		prettier.format(content, {
+			...(await prettier.resolveConfig(path)),
+			parser: "markdown",
+		}),
+	ruleDocTitleFormat: "name",
+};
+
+export default config;
+`,
+				],
+			},
+			options: optionsBase,
+		});
+
+		expect(actual).toEqual({
+			configEmoji: [
+				["recommended", "✅"],
+				["legacy-recommended", "✔️"],
+			],
+		});
 	});
 });
